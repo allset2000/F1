c7c43f8ab7839735703ed00eaa735963bc7f9b41
Jonathan Pobst <jpobst@entradahealth.com>
2014-01-29 16:26:14 -0600

    Add Greenway to static EHRVendor table.

--- HostedClient/Data/dbo.EHRVendors_Data.sql
+++ HostedClient/Data/dbo.EHRVendors_Data.sql
@@ -4,4 +4,5 @@ INSERT INTO [dbo].[EHRVendors] ([EHRVendorID], [Name], [CanAck]) VALUES (2, 'Ath
 INSERT INTO [dbo].[EHRVendors] ([EHRVendorID], [Name], [CanAck]) VALUES (3, 'NextGen', 0)
 INSERT INTO [dbo].[EHRVendors] ([EHRVendorID], [Name], [CanAck]) VALUES (4, 'Allscripts', 0)
 INSERT INTO [dbo].[EHRVendors] ([EHRVendorID], [Name], [CanAck]) VALUES (5, 'STAR Panel', 0)
+INSERT INTO [dbo].[EHRVendors] ([EHRVendorID], [Name], [CanAck]) VALUES (6, 'Greenway', 0)
 SET IDENTITY_INSERT [dbo].[EHRVendors] OFF
