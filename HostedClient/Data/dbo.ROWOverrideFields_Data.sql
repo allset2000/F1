b0f8085624c352cecf5f71641a14a8de85f75b77
Samuel Shoultz <sshoultz@entradahealth.com>
2014-08-05 16:52:44 -0500

    Modified ROWOverrideFields table to include EHRVendorId, added ROWOverrideFields data mapping / tracking.

---
+++ HostedClient/Data/dbo.ROWOverrideFields_Data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[ROWOverrideFields] ON
+INSERT INTO [dbo].[ROWOverrideFields] ([FieldID], [FieldName], [EHRVendorId]) VALUES (1, 'EHREncounterID', 6)
+INSERT INTO [dbo].[ROWOverrideFields] ([FieldID], [FieldName], [EHRVendorId]) VALUES (2, 'EHRDocumentTypeID', 6)
+INSERT INTO [dbo].[ROWOverrideFields] ([FieldID], [FieldName], [EHRVendorId]) VALUES (3, 'EHRDocumentID', 6)
+SET IDENTITY_INSERT [dbo].[ROWOverrideFields] OFF
--- HostedClient/Tables/dbo.ROWOverrideFields.sql
+++ HostedClient/Tables/dbo.ROWOverrideFields.sql
@@ -1,8 +1,11 @@
 CREATE TABLE [dbo].[ROWOverrideFields]
 (
 [FieldID] [smallint] NOT NULL IDENTITY(1, 1),
-[FieldName] [varchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+[FieldName] [varchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[EHRVendorId] [smallint] NOT NULL
 ) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[ROWOverrideFields] ADD CONSTRAINT [PK_ROWOverrideFields] PRIMARY KEY CLUSTERED  ([FieldID]) ON [PRIMARY]
+ALTER TABLE [dbo].[ROWOverrideFields] ADD
+CONSTRAINT [FK_ROWOverrideFields_EHRVendor] FOREIGN KEY ([EHRVendorId]) REFERENCES [dbo].[EHRVendors] ([EHRVendorID])
+ALTER TABLE [dbo].[ROWOverrideFields] ADD 
+CONSTRAINT [PK_ROWOverrideFields] PRIMARY KEY CLUSTERED  ([FieldID]) ON [PRIMARY]
 GO
