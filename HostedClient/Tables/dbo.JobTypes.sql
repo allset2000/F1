b2ebd3dc051c5c0a87dcc5f16f78b8806cb559b7
Samuel Shoultz <sshoultz@entradahealth.com>
2014-08-09 11:49:35 -0500

    Data changes on permissions, and new column in JobTypes

--- HostedClient/Tables/dbo.JobTypes.sql
+++ HostedClient/Tables/dbo.JobTypes.sql
@@ -8,12 +8,16 @@ CREATE TABLE [dbo].[JobTypes]
 [TddEnabled] [bit] NOT NULL CONSTRAINT [DF_JobTypes_TddEnabled] DEFAULT ((0)),
 [AckEnabled] [smallint] NOT NULL CONSTRAINT [DF_RulesJobs_AckEnabled] DEFAULT ((0)),
 [EHRDocumentTypeID] [varchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_JobTypes_EHRDocumentTypeID] DEFAULT (''),
+[EHRImageTypeID] [varchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_JobTypes_EHRImageTypeID] DEFAULT (''),
 [NoShowEnabled] [bit] NOT NULL CONSTRAINT [DF__JobTypes__NoShow__4C364F0E] DEFAULT ((0)),
 [DisableGenericJobs] [bit] NOT NULL CONSTRAINT [DF__JobTypes__Disabl__4D2A7347] DEFAULT ((0))
 ) ON [PRIMARY]
 ALTER TABLE [dbo].[JobTypes] ADD 
 CONSTRAINT [PK_JobTypes] PRIMARY KEY CLUSTERED  ([JobTypeID]) ON [PRIMARY]
 CREATE NONCLUSTERED INDEX [IX_JobTypes_ClinicID] ON [dbo].[JobTypes] ([ClinicID]) ON [PRIMARY]
+
+
+
 
 GO
 
