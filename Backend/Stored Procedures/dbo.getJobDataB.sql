7317d75a27d40cdf4850d123207fef88939c5602
rnarender <ramadheni.narender@adroitent.com>
2014-08-26 14:53:09 +0530

    1131 task: Get previous edit completed jobs data

---
+++ Backend/Stored Procedures/dbo.getJobDataB.sql
@@ -0,0 +1,26 @@
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+Create PROCEDURE [dbo].[getJobDataB] (
+   @JobNumber varchar(20)
+) AS
+	SELECT [DictatorID], [ClinicID], [ClinicName], [Location], '' AS LocationName,
+		   CONVERT(varchar, [AppointmentDate], 101) AS AppointmentDate,
+		   LTRIM(RTRIM(SUBSTRING(CONVERT(varchar, [AppointmentTime], 22), 9, 15))) AS AppointmentTime,
+		   [JobType], 
+		   [Stat] ,[CC], [Duration], 
+		   CONVERT(varchar, [DictationDate], 101) AS DictationDate,
+		   LTRIM(RTRIM(SUBSTRING(CONVERT(varchar, [DictationTime], 22), 9, 15))) AS DictationTime, 
+		   ISNULL(ParentJobNumber, '') AS ParentJobNumber
+	FROM   dbo.vwMedicalJobsB
+  WHERE (JobNumber = @JobNumber)
+RETURN
+
+GO
+
+
