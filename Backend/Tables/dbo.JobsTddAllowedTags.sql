b446be33741b1be37068fc448b8ba007f2ee3dba
Samuel Shoultz <sshoultz@entradahealth.com>
2014-09-09 07:14:40 -0500

    Expanded JobsTddAllowedTags.FieldName and ExpressNotesTags.FieldName from varchar(100) to varchar(1000) also pushing up the change to Jobs_Images (added clustered index / PK column)

--- Backend/Tables/dbo.JobsTddAllowedTags.sql
+++ Backend/Tables/dbo.JobsTddAllowedTags.sql
@@ -4,7 +4,7 @@ CREATE TABLE [dbo].[JobsTddAllowedTags]
 [SplitRuleID] [int] NOT NULL,
 [Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [Required] [bit] NOT NULL CONSTRAINT [DF_JobsTddAllowedTags_Required] DEFAULT ((0)),
-[FieldName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_JobsTddAllowedTags_FieldName] DEFAULT ('')
+[FieldName] [varchar] (1000) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_JobsTddAllowedTags_FieldName] DEFAULT ('')
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[JobsTddAllowedTags] ADD CONSTRAINT [PK_JobsTddAllowedTags] PRIMARY KEY CLUSTERED  ([ID]) ON [PRIMARY]
--- HostedClient/Tables/dbo.ExpressNotesTags.sql
+++ HostedClient/Tables/dbo.ExpressNotesTags.sql
@@ -4,7 +4,7 @@ CREATE TABLE [dbo].[ExpressNotesTags]
 [JobTypeID] [int] NOT NULL,
 [Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [Required] [bit] NOT NULL,
-[FieldName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+[FieldName] [varchar] (1000) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[ExpressNotesTags] ADD CONSTRAINT [PK_ExpressNotesTags] PRIMARY KEY CLUSTERED  ([TagID]) ON [PRIMARY]
