fe9f1fbc37254285f8a9dfb8352f1e3bf8e0c92b
Samuel Shoultz <sshoultz@entradahealth.com>
2014-12-01 11:40:50 -0600

    #3026# - New Sproces for creating/updating Contact / ContactRole records

---
+++ Backend/Stored Procedures/dbo.sp_CreateUpdateContact.sql
@@ -0,0 +1,64 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+-- =============================================
+-- Author: Sam Shoultz
+-- Create date: 12/1/2014
+-- Description: SP used to Create/Update Contact records by the AC (Editor, or Dictator)
+-- =============================================
+CREATE PROCEDURE [dbo].[sp_CreateUpdateContact](
+	@ID int,
+	@ContactType varchar(1),
+	@FullName varchar(254),
+	@FirstName varchar(48),
+	@MiddleName varchar(48),
+	@LastName varchar(48),
+	@Initials varchar(12),
+	@UserID varchar(48),
+	@Password varchar(48),
+	@ASPMembershipPwd varchar(48),
+	@UserSettings varchar(4096),
+	@EMail varchar(48),
+	@ContactStatus varchar(1),
+	@SecurityToken varchar(255)
+)  AS 
+BEGIN
+
+	IF NOT EXISTS (SELECT * FROM Contacts where ContactId = @ID)
+	BEGIN
+	
+		DECLARE @NewContactId int
+		-- Get new ContactId and update DBRules table with new id
+		UPDATE DbRules
+		SET @NewContactId = CurrentIdValue = CurrentIdValue + 1
+		WHERE (SourceName = 'Contacts') AND (DbRuleType = 'I')
+
+		-- Insert new record
+		INSERT INTO Contacts(ContactId,ContactType,FullName,FirstName,MiddleName,LastName,Initials,UserID,Password,ASPMembershipPwd,UserSettings,EMail,ContactStatus,SecurityToken)
+		VALUES(@NewContactId, @ContactType, @FullName, @FirstName, @MiddleName, @LastName, @Initials, @UserID, @Password, @ASPMembershipPwd, @UserSettings, @EMail, @ContactStatus, @SecurityToken)
+	END
+	ELSE
+	BEGIN
+		-- Record already exists, update values
+		UPDATE Contacts SET ContactType = @ContactType,
+							FullName = @FullName,
+							FirstName = @FirstName,
+							MiddleName = @MiddleName,
+							LastName = @LastName,
+							Initials = @Initials, 
+							UserID = @UserID,
+							Password = @Password,
+							ASPMembershipPwd = @ASPMembershipPwd,
+							UserSettings = @UserSettings,
+							EMail = @EMail,
+							ContactStatus = @ContactStatus,
+							SecurityToken = @SecurityToken
+		WHERE ContactId = @ID
+	END	
+
+END
+
+
+GO
---
+++ Backend/Stored Procedures/dbo.sp_CreateUpdateContactRoles.sql
@@ -0,0 +1,50 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+-- =============================================
+-- Author: Sam Shoultz
+-- Create date: 12/1/2014
+-- Description: SP used to Create/Update Contact Role records by the AC
+-- =============================================
+CREATE PROCEDURE [dbo].[sp_CreateUpdateContactRoles](
+	@ContactRoleId int,
+	@ContactId int,
+	@RoleId int,
+	@ClinicId int,
+	@ClinicsFilter varchar(500),
+	@DictatorsFilter varchar(500),
+	@JobsFilter varchar(255),
+	@RoleStatus char(1) 
+)  AS 
+BEGIN
+
+	IF NOT EXISTS (SELECT * FROM ContactRoles where ContactRoleId = @ContactRoleId)
+	BEGIN
+	
+		DECLARE @NewRoleId int
+		-- Get new ContactRoleId and update DBRules table with new id
+		UPDATE DbRules
+		SET @NewRoleId = CurrentIdValue = CurrentIdValue + 1
+		WHERE (SourceName = 'ContactRoles') AND (DbRuleType = 'I')
+
+		-- Insert new record
+		INSERT INTO ContactRoles(ContactRoleId, ContactId, RoleId, ClinicId, ClinicsFilter, DictatorsFilter, JobsFilter, RoleStatus) 
+		VALUES (@NewRoleId, @ContactId, @RoleId, @ClinicId, @ClinicsFilter, @DictatorsFilter, @JobsFilter, @RoleStatus)
+	END
+	ELSE
+	BEGIN
+		-- Record already exists, update values
+		UPDATE ContactRoles	SET RoleId = @RoleId,
+								ClinicId = @ClinicId,
+								ClinicsFilter = @ClinicsFilter,
+								DictatorsFilter = @DictatorsFilter,
+								JobsFilter = @JobsFilter,
+								RoleStatus = @RoleStatus
+		WHERE ContactRoleId = @ContactRoleId
+	END	
+END
+
+
+GO
