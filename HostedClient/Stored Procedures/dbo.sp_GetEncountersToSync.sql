c9ac7d4ec553f1cd890585efa1b588ea3b9e4f82
Samuel Shoultz <sshoultz@entradahealth.com>
2014-12-03 12:21:15 -0600

    #0000# new queries for optimizing dictate api / mobile sync

---
+++ HostedClient/Stored Procedures/dbo.sp_GetEncountersToSync.sql
@@ -0,0 +1,41 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author: Sam Shoultz
+-- Create date: 12/3/2014
+-- Description: SP called from DictateAPI to pull Dictations to sync on mobile
+-- =============================================
+CREATE PROCEDURE [dbo].[sp_GetEncountersToSync] (
+	 @DictatorId int,
+	 @MaxFutureDays int
+) AS 
+BEGIN
+	IF (@MaxFutureDays > 0)
+	BEGIN	 
+		SELECT E.* 
+		FROM Dictations D
+			INNER JOIN Jobs J ON D.JobID = J.JobID
+			INNER JOIN Encounters E ON J.EncounterID = E.EncounterID
+			INNER JOIN Queue_Users QU on QU.QueueID = D.QueueID
+			INNER JOIN Queues Q on Q.QueueID = QU.QueueID
+		WHERE (D.DictatorID = @DictatorId or QU.DictatorID = @DictatorId)
+			  AND D.Status in (200,100)
+			  AND DATEDIFF (D, GETDATE (), E.AppointmentDate) <= @MaxFutureDays
+			  AND Q.Deleted = 0
+	END
+	ELSE
+	BEGIN
+		SELECT E.* 
+		FROM Dictations D
+			INNER JOIN Jobs J ON D.JobID = J.JobID
+			INNER JOIN Encounters E ON J.EncounterID = E.EncounterID
+			INNER JOIN Queue_Users QU on QU.QueueID = D.QueueID
+			INNER JOIN Queues Q on Q.QueueID = QU.QueueID
+		WHERE D.Status in (200,100)
+			  AND (D.DictatorID = @DictatorId or QU.DictatorID = @DictatorId)
+			  AND Q.Deleted = 0
+	END
+END
+GO
