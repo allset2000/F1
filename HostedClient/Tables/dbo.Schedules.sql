7051fd58a3eb06ef659006133902a9ddf8610e50
DDorsey <ddorsey@entradahealth.com>
2014-12-15 10:46:26 -0600

    Revert "Reverse change"
    This reverts commit 3862ff4510cee30151bc512e73425ffa3bee7b08.

---
+++ HostedClient/Tables/dbo.Schedules.sql
@@ -0,0 +1,48 @@
+CREATE TABLE [dbo].[Schedules]
+(
+[ScheduleID] [bigint] NOT NULL IDENTITY(1, 1),
+[ClinicID] [smallint] NOT NULL,
+[AppointmentDate] [datetime] NOT NULL,
+[PatientID] [int] NOT NULL,
+[AppointmentID] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Schedules_AppointmentID] DEFAULT (''),
+[EHREncounterID] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL CONSTRAINT [DF_Schedules_EncounterID] DEFAULT (''),
+[Attending] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL CONSTRAINT [DF_Schedules_Attending] DEFAULT (''),
+[LocationID] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Schedules_LocationID] DEFAULT (''),
+[LocationName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Schedules_LocationName] DEFAULT (''),
+[ReasonID] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Schedules_ReasonID] DEFAULT (''),
+[ReasonName] [varchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Schedules_ReasonName] DEFAULT (''),
+[ResourceID] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Schedules_ResourceID] DEFAULT (''),
+[ResourceName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Schedules_ResourceName] DEFAULT (''),
+[Status] [smallint] NOT NULL,
+[AdditionalData] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[RowProcessed] [int] NOT NULL CONSTRAINT [DF_Schedules_RowProcessed] DEFAULT ((0)),
+[ReferringID] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ReferringName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[AttendingFirst] [varchar] (120) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[AttendingLast] [varchar] (120) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ChangedOn] [datetime] NULL CONSTRAINT [DF_Schedules_ChangedOn] DEFAULT (getdate()),
+[Type] [varchar] (1) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Schedules_Type] DEFAULT ('S')
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+CREATE NONCLUSTERED INDEX [IDX_Schedules_ClinicIDAttending] ON [dbo].[Schedules] ([ClinicID], [Attending], [AttendingLast], [AttendingFirst]) ON [PRIMARY]
+
+
+
+
+
+CREATE NONCLUSTERED INDEX [LocationID] ON [dbo].[Schedules] ([LocationID]) ON [PRIMARY]
+
+CREATE NONCLUSTERED INDEX [ReasonID] ON [dbo].[Schedules] ([ReasonID]) ON [PRIMARY]
+
+CREATE NONCLUSTERED INDEX [ResourceID] ON [dbo].[Schedules] ([ResourceID]) ON [PRIMARY]
+
+CREATE NONCLUSTERED INDEX [IX_Schedules_1] ON [dbo].[Schedules] ([AppointmentID]) ON [PRIMARY]
+
+CREATE NONCLUSTERED INDEX [IX_Schedules] ON [dbo].[Schedules] ([ClinicID]) ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[Schedules] ADD CONSTRAINT [PK_Schedules] PRIMARY KEY CLUSTERED  ([ScheduleID]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_RowProcessed_ClinicID_AppointmentID_WithINC_ScheduleID_AppointmentDate] ON [dbo].[Schedules] ([RowProcessed], [ClinicID], [AppointmentID]) INCLUDE ([AppointmentDate], [ScheduleID]) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Schedules] ADD CONSTRAINT [FK_Schedules_Clinics] FOREIGN KEY ([ClinicID]) REFERENCES [dbo].[Clinics] ([ClinicID])
+GO
