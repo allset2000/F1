0696028a00619e4ff4c7b45a57688094484d98ce
Samuel Shoultz <sshoultz@entradahealth.com>
2015-01-09 11:04:27 -0600

    Final CC.1 DB Changes to hosted db

--- HostedClient/Tables/dbo.AuditLogDictateApi.sql
+++ HostedClient/Tables/dbo.AuditLogDictateApi.sql
@@ -2,7 +2,7 @@ CREATE TABLE [dbo].[AuditLogDictateApi]
 (
 [LogID] [bigint] NOT NULL IDENTITY(1, 1),
 [DictatorID] [int] NOT NULL,
-[Operation] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Operation] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [Parameters] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [OperationTime] [datetime] NOT NULL,
 [ExecutionTime] [int] NOT NULL,
--- HostedClient/Tables/dbo.JobTypes.sql
+++ HostedClient/Tables/dbo.JobTypes.sql
@@ -11,7 +11,8 @@ CREATE TABLE [dbo].[JobTypes]
 [EHRImageTypeID] [varchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_JobTypes_EHRImageTypeID] DEFAULT (''),
 [NoShowEnabled] [bit] NOT NULL CONSTRAINT [DF__JobTypes__NoShow__4C364F0E] DEFAULT ((0)),
 [DisableGenericJobs] [bit] NOT NULL CONSTRAINT [DF__JobTypes__Disabl__4D2A7347] DEFAULT ((0)),
-[AllowEncounterSearch] [bit] NOT NULL CONSTRAINT [DF_JobTypes_AllowEncounterSearch] DEFAULT ((0))
+[AllowEncounterSearch] [bit] NOT NULL CONSTRAINT [DF_JobTypes_AllowEncounterSearch] DEFAULT ((0)),
+[AllowNotifications] [bit] NOT NULL CONSTRAINT [DF_JobTypes_AllowNotifications] DEFAULT ((0))
 ) ON [PRIMARY]
 ALTER TABLE [dbo].[JobTypes] ADD 
 CONSTRAINT [PK_JobTypes] PRIMARY KEY CLUSTERED  ([JobTypeID]) ON [PRIMARY]
