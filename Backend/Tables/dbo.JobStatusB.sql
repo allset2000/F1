62eec0174208f784090159bb8df355e1240ac1c0
DDorsey <ddorsey@entradahealth.com>
2015-01-23 16:06:05 -0600

    #23683 - Index change on JobStatusB table

--- Backend/Tables/dbo.JobStatusB.sql
+++ Backend/Tables/dbo.JobStatusB.sql
@@ -5,9 +5,11 @@ CREATE TABLE [dbo].[JobStatusB]
 [StatusDate] [datetime] NULL,
 [Path] [varchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
+CREATE NONCLUSTERED INDEX [IX_Status_INC_JobNumber] ON [dbo].[JobStatusB] ([Status]) INCLUDE ([JobNumber], [StatusDate]) ON [PRIMARY]
+
 ALTER TABLE [dbo].[JobStatusB] ADD 
 CONSTRAINT [PK_JobStatusB] PRIMARY KEY CLUSTERED  ([JobNumber] DESC) ON [PRIMARY]
-CREATE NONCLUSTERED INDEX [IX_Status_INC_JobNumber] ON [dbo].[JobStatusB] ([Status]) INCLUDE ([JobNumber]) ON [PRIMARY]
+
 
 ALTER TABLE [dbo].[JobStatusB] ADD
 CONSTRAINT [FK_JobStatusB_StatusCodes] FOREIGN KEY ([Status]) REFERENCES [dbo].[StatusCodes] ([StatusID]) ON DELETE CASCADE ON UPDATE CASCADE
