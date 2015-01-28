20733ef9978fb77d2ca782a40a6749b5e1ae65ac
DDorsey <ddorsey@entradahealth.com>
2015-01-28 10:18:14 -0600

    CC 1032 - Update datatype for column in RulesJobs and RulesDictations

--- HostedClient/Tables/dbo.RulesJobs.sql
+++ HostedClient/Tables/dbo.RulesJobs.sql
@@ -1,14 +1,15 @@
 CREATE TABLE [dbo].[RulesJobs]
 (
 [RuleID] [smallint] NOT NULL,
-[ActionID] [smallint] NOT NULL IDENTITY(1, 1),
+[ActionID] [int] NOT NULL IDENTITY(1, 1),
 [DictatorID] [int] NULL,
 [QueueID] [int] NULL,
 [ProviderID] [bigint] NULL
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[RulesJobs] ADD 
+CONSTRAINT [PK_Rules_Jobs] PRIMARY KEY CLUSTERED  ([RuleID], [ActionID]) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[RulesJobs] ADD CONSTRAINT [PK_Rules_Jobs] PRIMARY KEY CLUSTERED  ([RuleID], [ActionID]) ON [PRIMARY]
-GO
+
 ALTER TABLE [dbo].[RulesJobs] ADD CONSTRAINT [FK_Rules_Jobs_Dictators] FOREIGN KEY ([DictatorID]) REFERENCES [dbo].[Dictators] ([DictatorID])
 GO
 ALTER TABLE [dbo].[RulesJobs] ADD CONSTRAINT [FK_Rules_Jobs_Rules_Definition] FOREIGN KEY ([RuleID]) REFERENCES [dbo].[Rules] ([RuleID]) ON DELETE CASCADE
