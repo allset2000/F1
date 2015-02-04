82b34eb586726a27886661e9d9a7a0d7b3690994
Samuel Shoultz <sshoultz@entradahealth.com>
2015-02-04 12:14:23 -0600

    #3413# - updated columns from small int to int

--- HostedClient/Tables/dbo.RulesReasonsXref.sql
+++ HostedClient/Tables/dbo.RulesReasonsXref.sql
@@ -1,14 +1,15 @@
 CREATE TABLE [dbo].[RulesReasonsXref]
 (
 [RuleID] [smallint] NOT NULL,
-[ActionID] [smallint] NOT NULL IDENTITY(1, 1),
+[ActionID] [int] NOT NULL IDENTITY(1, 1),
 [ReasonID] [smallint] NULL
 ) ON [PRIMARY]
-CREATE NONCLUSTERED INDEX [IX_RulesReasonsXref_ReasonID_includes] ON [dbo].[RulesReasonsXref] ([ReasonID]) INCLUDE ([RuleID]) ON [PRIMARY]
+ALTER TABLE [dbo].[RulesReasonsXref] ADD 
+CONSTRAINT [PK_Rules_ReasonXref] PRIMARY KEY CLUSTERED  ([RuleID], [ActionID]) ON [PRIMARY]
+
 
 GO
-ALTER TABLE [dbo].[RulesReasonsXref] ADD CONSTRAINT [PK_Rules_ReasonXref] PRIMARY KEY CLUSTERED  ([RuleID], [ActionID]) ON [PRIMARY]
-GO
+
 ALTER TABLE [dbo].[RulesReasonsXref] ADD CONSTRAINT [FK_Rulesreasons_ReasonsXref_Rules] FOREIGN KEY ([ReasonID]) REFERENCES [dbo].[RulesReasons] ([ID])
 GO
 ALTER TABLE [dbo].[RulesReasonsXref] ADD CONSTRAINT [FK_Rules_ReasonXref_Rules] FOREIGN KEY ([RuleID]) REFERENCES [dbo].[Rules] ([RuleID])
