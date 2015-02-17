8ca24f2e5b922e1dc29282dc8e344c3e1c7917d2
Samuel Shoultz <sshoultz@entradahealth.com>
2015-02-17 14:18:27 -0600

    Revert "#2782# Supporting DB changes for new Centralized User MGM"
    This reverts commit 3df31b4c61f799cc2169eef3328482307090624c.

--- HostedClient/Data/dbo.Applications_Data.sql
+++
@@ -1,7 +0,0 @@
-SET IDENTITY_INSERT [dbo].[Applications] ON
-INSERT INTO [dbo].[Applications] ([ApplicationId], [Description], [PermissionEnabled], [IsDeleted], [DateCreated], [DateUpdated]) VALUES (1, 'ExpressLink', 0, 0, NULL, NULL)
-INSERT INTO [dbo].[Applications] ([ApplicationId], [Description], [PermissionEnabled], [IsDeleted], [DateCreated], [DateUpdated]) VALUES (2, 'MirthConnect', 0, 0, NULL, NULL)
-INSERT INTO [dbo].[Applications] ([ApplicationId], [Description], [PermissionEnabled], [IsDeleted], [DateCreated], [DateUpdated]) VALUES (3, 'Downloader', 0, 0, NULL, NULL)
-INSERT INTO [dbo].[Applications] ([ApplicationId], [Description], [PermissionEnabled], [IsDeleted], [DateCreated], [DateUpdated]) VALUES (4, 'Entrada Mobile', 1, 0, '2015-01-14 14:11:50.537', '2015-01-14 14:11:50.537')
-INSERT INTO [dbo].[Applications] ([ApplicationId], [Description], [PermissionEnabled], [IsDeleted], [DateCreated], [DateUpdated]) VALUES (5, 'Admin Console', 1, 0, '2015-01-14 14:12:03.320', '2015-01-14 14:12:03.320')
-SET IDENTITY_INSERT [dbo].[Applications] OFF
--- HostedClient/Tables/dbo.Applications.sql
+++ HostedClient/Tables/dbo.Applications.sql
@@ -1,11 +1,7 @@
 CREATE TABLE [dbo].[Applications]
 (
 [ApplicationId] [int] NOT NULL IDENTITY(1, 1),
-[Description] [varchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[PermissionEnabled] [bit] NOT NULL CONSTRAINT [DF_Applications_PermissionEnabled] DEFAULT ((0)),
-[IsDeleted] [bit] NOT NULL CONSTRAINT [DF_Applications_IsDeleted] DEFAULT ((0)),
-[DateCreated] [datetime] NULL,
-[DateUpdated] [datetime] NULL
+[Description] [varchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Applications] ADD CONSTRAINT [PK_Applications] PRIMARY KEY CLUSTERED  ([ApplicationId]) ON [PRIMARY]
