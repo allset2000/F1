6c3066ad4e31eebd48b5893bc621b776df119e8f
MBayramov <mbayramov@entradahealth.com>
2015-02-24 11:06:49 -0600

    Added PatientID field to MessageThreads table  that will reflect to what patient the thread is related to

--- HostedClient/Tables/dbo.MessageThreads.sql
+++ HostedClient/Tables/dbo.MessageThreads.sql
@@ -4,13 +4,17 @@ CREATE TABLE [dbo].[MessageThreads]
 [ThreadID] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [ThreadOwnerID] [int] NOT NULL,
 [ThreadOwnerClinicID] [smallint] NOT NULL,
+[PatientID] [int] NOT NULL,
 [CreatedDate] [datetime] NOT NULL,
 [UpdatedDate] [datetime] NULL,
-[IsActive] [bit] NOT NULL CONSTRAINT [DF__MessageTh__IsAct__0FC23DAB] DEFAULT ((1))
+[IsActive] [bit] NOT NULL CONSTRAINT [DF__MessageTh__IsAct__20ECC9AD] DEFAULT ((1))
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[MessageThreads] ADD 
+CONSTRAINT [PK_MessageThreads] PRIMARY KEY CLUSTERED  ([MessageThreadID]) ON [PRIMARY]
+ALTER TABLE [dbo].[MessageThreads] ADD
+CONSTRAINT [FK_MessageThreads_Patients] FOREIGN KEY ([PatientID]) REFERENCES [dbo].[Patients] ([PatientID])
 GO
-ALTER TABLE [dbo].[MessageThreads] ADD CONSTRAINT [PK_MessageThreads] PRIMARY KEY CLUSTERED  ([MessageThreadID]) ON [PRIMARY]
-GO
+
 ALTER TABLE [dbo].[MessageThreads] ADD CONSTRAINT [FK_MessageThreads_Clinics] FOREIGN KEY ([ThreadOwnerClinicID]) REFERENCES [dbo].[Clinics] ([ClinicID])
 GO
 ALTER TABLE [dbo].[MessageThreads] ADD CONSTRAINT [FK_MessageThreads_Users] FOREIGN KEY ([ThreadOwnerID]) REFERENCES [dbo].[Users] ([UserID])
