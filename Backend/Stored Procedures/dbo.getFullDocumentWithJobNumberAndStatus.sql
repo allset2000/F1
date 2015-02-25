848a7502625a8032c28d2672a396e2baa932495c
rnarender <ramadheni.narender@adroitent.com>
2015-02-25 21:04:57 +0530

    #3589# Updated the SP: getFullDocumentWithJobNumberAndStatus and table DbQueryParameters
    This is related to Audit trails jobs documents

--- Backend/Stored Procedures/dbo.getFullDocumentWithJobNumberAndStatus.sql
+++ Backend/Stored Procedures/dbo.getFullDocumentWithJobNumberAndStatus.sql
@@ -1,7 +1,7 @@
 USE [Entrada]
 GO
 
-/****** Object:  StoredProcedure [dbo].[getFullDocumentWithJobNumberAndStatus]    Script Date: 1/21/2015 3:55:51 AM ******/
+/****** Object:  StoredProcedure [dbo].[getFullDocumentWithJobNumberAndStatus]    Script Date: 2/25/2015 9:08:38 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -11,42 +11,38 @@ GO
 
 CREATE PROCEDURE [dbo].[getFullDocumentWithJobNumberAndStatus] (
    @JobNumber varchar(20),
-   @Status varchar(20),
+   @Status smallint,
    @DocID int
 )
 AS
 BEGIN
-	SET NOCOUNT ON;
-	
-	WITH HistoryInfo AS((
-	SELECT dbo.Jobs.JobNumber, dbo.Jobs_Documents_History.Status, dbo.Jobs_Documents_History.Doc, 
-	       dbo.StatusCodes.StatusName, dbo.StatusCodes.StatusClass, dbo.StatusCodes.StatusStage,
-	       dbo.Jobs_Documents_History.Username AS QAName,
-	        dbo.Jobs_Documents_History.DocumentID,dbo.Jobs_Documents.Username AS EditorName
-	FROM   dbo.Jobs INNER JOIN
-						  dbo.Jobs_Documents_History ON dbo.Jobs.JobNumber=dbo.Jobs_Documents_History.JobNumber
-						  INNER JOIN
-						  dbo.StatusCodes ON dbo.Jobs_Documents_History.Status = dbo.StatusCodes.StatusID
-						  INNER JOIN
-						  dbo.Jobs_Documents ON dbo.jobs.JobNumber=dbo.Jobs_Documents.JobNumber				  						 						  
-	WHERE dbo.jobs.JobNumber = @JobNumber 
-)
-UNION
-(
-	SELECT  dbo.jobs.JobNumber,dbo.JobTracking.Status,dbo.Jobs_Documents.Doc,
-		dbo.StatusCodes.StatusName, dbo.StatusCodes.StatusClass, dbo.StatusCodes.StatusStage,
-		dbo.Jobs_Documents.Username AS QAName, dbo.Jobs_Documents.DocumentId, dbo.jobs.EditorID AS EditorName	
-	FROM dbo.Jobs INNER JOIN 
-		dbo.Jobs_Documents ON dbo.jobs.JobNumber=dbo.Jobs_Documents.JobNumber
-		INNER JOIN dbo.JobTracking ON dbo.JobTracking.JobNumber=dbo.jobs.JobNumber
-			and dbo.Jobs.JobStatus=dbo.JobTracking.Status
-		 INNER JOIN dbo.StatusCodes ON dbo.jobs.JobStatus = dbo.StatusCodes.StatusID
-	 WHERE  dbo.Jobs.JobNumber = @JobNumber AND (dbo.JobTracking.Status IN (100,160,180,210,360,240,250))
-	 )
-	 )
-SELECT * FROM HistoryInfo WHERE Status=@Status and DocumentID=@DocID
+    SET NOCOUNT ON;
+ 
+    SELECT dbo.Jobs.JobNumber, dbo.Jobs_Documents_History.Status, dbo.Jobs_Documents_History.Doc, 
+            dbo.StatusCodes.StatusName, dbo.StatusCodes.StatusClass, dbo.StatusCodes.StatusStage,
+            dbo.Jobs_Documents_History.Username AS QAName,
+            dbo.Jobs_Documents_History.DocumentID,dbo.Jobs_Documents.Username AS EditorName
+    FROM   dbo.Jobs INNER JOIN
+                dbo.Jobs_Documents_History ON dbo.Jobs.JobNumber=dbo.Jobs_Documents_History.JobNumber
+                INNER JOIN
+                dbo.StatusCodes ON dbo.Jobs_Documents_History.Status = dbo.StatusCodes.StatusID
+                INNER JOIN
+                dbo.Jobs_Documents ON dbo.jobs.JobNumber=dbo.Jobs_Documents.JobNumber                                                                                                                                                                                                                                   
+    WHERE dbo.jobs.JobNumber = @JobNumber and 
+                                    dbo.Jobs_Documents_History.Status = @Status and dbo.Jobs_Documents_History.DocumentID = @DocID
+UNION ALL
+ 
+    SELECT  dbo.jobs.JobNumber,dbo.JobTracking.Status,dbo.Jobs_Documents.Doc,
+                    dbo.StatusCodes.StatusName, dbo.StatusCodes.StatusClass, dbo.StatusCodes.StatusStage,
+                    dbo.Jobs_Documents.Username AS QAName, dbo.Jobs_Documents.DocumentId, dbo.jobs.EditorID AS EditorName         
+    FROM dbo.Jobs INNER JOIN 
+                    dbo.Jobs_Documents ON dbo.jobs.JobNumber=dbo.Jobs_Documents.JobNumber
+                    INNER JOIN dbo.JobTracking ON dbo.JobTracking.JobNumber=dbo.jobs.JobNumber
+                                    and dbo.Jobs.JobStatus=dbo.JobTracking.Status
+                    INNER JOIN dbo.StatusCodes ON dbo.jobs.JobStatus = dbo.StatusCodes.StatusID
+    WHERE  dbo.Jobs.JobNumber = @JobNumber AND (dbo.JobTracking.Status IN (100,160,180,210,360,240,250)) and
+                                    dbo.JobTracking.Status = @Status and dbo.jobs_documents.DocumentID = @DocID
+               
 END
 
 GO
-
-
--- Backend/Stored Procedures/dbo.spv_UpgradeScript_post_CC2.sql
+++ Backend/Stored Procedures/dbo.spv_UpgradeScript_post_CC2.sql
@@ -17,11 +17,12 @@ IF NOT EXISTS(select * from DbQueryParameters where QueryName = 'getFullDocument
 		[ParameterOracleType], [ParameterDirection], [ParameterSize], [ParameterScale], [ParameterPrecision], [ParameterDefaultValue]) 
 		VALUES (N'getFullDocumentWithJobNumberAndStatus', 0, N'JobNumber', 22, 15, 200, 22, 1, 20, 0, 0, N'')
 	END
-IF NOT EXISTS(select * from DbQueryParameters where QueryName = 'getFullDocumentWithJobNumberAndStatus' and ParameterIndex = 1 and ParameterName = 'Status')
+IF NOT EXISTS(select * from DbQueryParameters where QueryName = 'getFullDocumentWithJobNumberAndStatus' and ParameterIndex = 1 and ParameterName = 'Status'
+			and ParameterSqlType = 16 and ParameterSize = 0)
 	BEGIN
 		INSERT INTO [dbo].[DbQueryParameters] ([QueryName], [ParameterIndex], [ParameterName], [ParameterSqlType], [ParameterMySqlType], [ParameterOleDbType], 
 		[ParameterOracleType], [ParameterDirection], [ParameterSize], [ParameterScale], [ParameterPrecision], [ParameterDefaultValue]) 
-		VALUES (N'getFullDocumentWithJobNumberAndStatus', 1, N'Status', 22, 15, 200, 22, 1, 20, 0, 0, N'')
+		VALUES (N'getFullDocumentWithJobNumberAndStatus', 1, N'Status', 16, 2, 2, 27, 1, 0, 0, 0, N'')
 	END
 IF NOT EXISTS(select * from DbQueryParameters where QueryName = 'getFullDocumentWithJobNumberAndStatus' and ParameterIndex = 2 and ParameterName = 'DocID')
 	BEGIN
