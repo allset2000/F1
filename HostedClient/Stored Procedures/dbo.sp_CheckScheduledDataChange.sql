c00f8a9f65be57b2f830da9c03df589032fc2a74
DDorsey <ddorsey@entradahealth.com>
2015-03-04 14:35:16 -0600

    Adding missing stored proc - sp_CheckScheduledDataChange

---
+++ HostedClient/Stored Procedures/dbo.sp_CheckScheduledDataChange.sql
@@ -0,0 +1,47 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+-- =============================================
+-- Author:		<Michael Cardwell>
+-- Create date: <1/12/2015>
+-- Description:	<Checks to see if any appointmetn data has changed.  Sends 1 if true 0 if false>
+-- =============================================
+CREATE PROCEDURE [dbo].[sp_CheckScheduledDataChange]
+	@ClinicID SMALLINT,
+	@AppointmentID VARCHAR(100),
+	@appointmentDate DATETIME,
+	@EHREncounterID  VARCHAR(50),
+	@LocationID VARCHAR(50),
+	@ReasonID VARCHAR(50),
+	@ResourceID VARCHAR(50),
+	@Status SMALLINT,
+	@MRN VARCHAR(36)
+AS
+BEGIN
+
+DECLARE @result INT
+
+
+
+		SELECT @result = CASE
+					WHEN @AppointmentDate != S.AppointmentDate OR 
+						 @EHREncounterID != S.EHREncounterID OR
+						 @LocationID != S.LocationID OR 
+						 @ReasonID != S.ReasonID OR 
+						 @ResourceID != S.ResourceID OR 
+						 @Status != S.[Status] OR 
+						 @MRN != P.MRN
+					THEN 1
+					ELSE 0 
+				END
+		FROM Schedules AS S INNER JOIN Patients AS P ON S.patientID = P.PatientID
+		WHERE S.AppointmentID = @AppointmentID AND 
+			  S.ClinicID = @ClinicID
+	
+SELECT ISNULL(@result,1)
+
+END
+
+GO
