cddf0f18cbbfa67683e039869e0c8f86d6272471
DDorsey <ddorsey@entradahealth.com>
2015-03-11 12:58:37 -0500

    New Index creation on EditorLogs

--- Backend/Tables/dbo.EditorLogs.sql
+++ Backend/Tables/dbo.EditorLogs.sql
@@ -9,6 +9,8 @@ CREATE TABLE [dbo].[EditorLogs]
 [ExceptionMessage] [varchar] (1024) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [SessionID] [varchar] (128) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY]
+CREATE NONCLUSTERED INDEX [IX_EditorLogs_JobNumber] ON [dbo].[EditorLogs] ([JobNumber] DESC) ON [PRIMARY]
+
 CREATE NONCLUSTERED INDEX [IX_EditorLogs_OperationTime] ON [dbo].[EditorLogs] ([OperationTime]) ON [PRIMARY]
 
 ALTER TABLE [dbo].[EditorLogs] ADD 
