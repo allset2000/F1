be30bc6e2746b503bf02939cad71f7ba7618c01a
rnarender <ramadheni.narender@adroitent.com>
2015-03-30 12:29:24 +0530

    #2356# Added sp_UpdateJobProcessFailureCount SP to hosted DB

---
+++ HostedClient/Stored Procedures/sp_UpdateJobProcessFailureCount.sql
@@ -0,0 +1,25 @@
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author: Narender
+-- Create date: 03/27/2015
+-- Description: SP Used to update the JobProcessFailureCount in jobs table
+-- =============================================
+CREATE PROCEDURE [dbo].[sp_UpdateJobProcessFailureCount]
+ @jobNumber VARCHAR(20)
+ AS
+BEGIN
+ declare @ProcessFailureCount smallint
+
+ IF(( select ProcessFailureCount from Jobs where JobNumber = @jobNumber) is NOT NULL)
+		UPDATE Jobs SET @ProcessFailureCount = ProcessFailureCount = ProcessFailureCount + 1 WHERE (JobNumber = @jobNumber)
+ ELSE
+		UPDATE Jobs SET  ProcessFailureCount = 1 WHERE (JobNumber = @jobNumber)
+END
+
+GO
+
+
