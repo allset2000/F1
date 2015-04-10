58ed5e81826b02055d22abd601b3e21a8995ecdc
MBayramov <mbayramov@entradahealth.com>
2015-04-10 07:51:10 -0500

    reformat

--- HostedClient/Tables/dbo.LogExceptions.sql
+++ HostedClient/Tables/dbo.LogExceptions.sql
@@ -2,13 +2,13 @@ CREATE TABLE [dbo].[LogExceptions]
 (
 [LogExceptionID] [int] NOT NULL IDENTITY(1, 1),
 [LogConfigurationID] [int] NOT NULL,
-[MainClassName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[MainMethodName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[ErrorThrownAtClassName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[ErrorThrownAtMethodName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[MainClassName] [varchar] (8000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[MainMethodName] [varchar] (8000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ErrorThrownAtClassName] [varchar] (8000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ErrorThrownAtMethodName] [varchar] (8000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [ExceptionMessage] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[InnerError] [varchar] (1000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[StackTrace] [varchar] (1000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[InnerError] [varchar] (8000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[StackTrace] [varchar] (8000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [ErrorCreatedDate] [datetime] NOT NULL CONSTRAINT [DF__LogExcept__Error__70F39DC8] DEFAULT (getdate()),
 [ErrorWrittenDate] [datetime] NOT NULL CONSTRAINT [DF__LogExcept__Error__71E7C201] DEFAULT (getdate())
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
