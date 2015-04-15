22a7b5bfdb75e57bc9985513e4dbb92f1d448bbe
santhosh <smukkamala@entradahealth.com>
2015-04-15 17:25:52 +0530

    #2850# - Missed table script

--- Backend/Tables/dbo.ClinicsMessagesRules.sql
+++ Backend/Tables/dbo.ClinicsMessagesRules.sql
@@ -1,5 +1,4 @@
-CREATE TABLE [dbo].[ClinicsMessagesRules]
-(
+CREATE TABLE [dbo].[ClinicsMessagesRules](
 [MessageRuleId] [int] NOT NULL,
 [MessageTypeId] [int] NOT NULL,
 [ClinicID] [smallint] NOT NULL,
@@ -13,35 +12,67 @@ CREATE TABLE [dbo].[ClinicsMessagesRules]
 [SendTo] [varchar] (4000) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [StatJobFrequency] [decimal] (8, 2) NOT NULL,
 [NoStatJobFrequency] [decimal] (8, 2) NOT NULL
+	[NoStatJobFrequency] [decimal](8, 2) NOT NULL,
+	,[UserID] [int] NULL,
+	[NotificationTypeID] [int] NULL,
+ CONSTRAINT [PK_CRMessageRules] PRIMARY KEY CLUSTERED 
+(
+	[MessageRuleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
-GRANT SELECT ON  [dbo].[ClinicsMessagesRules] TO [bliddle]
-GRANT INSERT ON  [dbo].[ClinicsMessagesRules] TO [bliddle]
-GRANT UPDATE ON  [dbo].[ClinicsMessagesRules] TO [bliddle]
-GRANT SELECT ON  [dbo].[ClinicsMessagesRules] TO [cgulley]
-GRANT INSERT ON  [dbo].[ClinicsMessagesRules] TO [cgulley]
-GRANT UPDATE ON  [dbo].[ClinicsMessagesRules] TO [cgulley]
-GRANT SELECT ON  [dbo].[ClinicsMessagesRules] TO [dhill]
-GRANT INSERT ON  [dbo].[ClinicsMessagesRules] TO [dhill]
-GRANT UPDATE ON  [dbo].[ClinicsMessagesRules] TO [dhill]
-GRANT SELECT ON  [dbo].[ClinicsMessagesRules] TO [ezobel]
-GRANT INSERT ON  [dbo].[ClinicsMessagesRules] TO [ezobel]
-GRANT UPDATE ON  [dbo].[ClinicsMessagesRules] TO [ezobel]
-GRANT TAKE OWNERSHIP ON  [dbo].[ClinicsMessagesRules] TO [jsteidinger]
-GRANT INSERT ON  [dbo].[ClinicsMessagesRules] TO [jsteidinger]
-GRANT SELECT ON  [dbo].[ClinicsMessagesRules] TO [mmoscoso]
-GRANT INSERT ON  [dbo].[ClinicsMessagesRules] TO [mmoscoso]
-GRANT UPDATE ON  [dbo].[ClinicsMessagesRules] TO [mmoscoso]
-GRANT SELECT ON  [dbo].[ClinicsMessagesRules] TO [nwest]
-GRANT INSERT ON  [dbo].[ClinicsMessagesRules] TO [nwest]
-GRANT UPDATE ON  [dbo].[ClinicsMessagesRules] TO [nwest]
-GRANT SELECT ON  [dbo].[ClinicsMessagesRules] TO [rspears]
-GRANT INSERT ON  [dbo].[ClinicsMessagesRules] TO [rspears]
-GRANT UPDATE ON  [dbo].[ClinicsMessagesRules] TO [rspears]
-GRANT SELECT ON  [dbo].[ClinicsMessagesRules] TO [rwilson]
-GRANT INSERT ON  [dbo].[ClinicsMessagesRules] TO [rwilson]
-GRANT UPDATE ON  [dbo].[ClinicsMessagesRules] TO [rwilson]
-GO
 
-ALTER TABLE [dbo].[ClinicsMessagesRules] ADD CONSTRAINT [PK_CRMessageRules] PRIMARY KEY CLUSTERED  ([MessageRuleId]) ON [PRIMARY]
+GO
+SET ANSI_PADDING OFF
+GO
+GRANT INSERT ON [dbo].[ClinicsMessagesRules] TO [bliddle] AS [dbo]
+GO
+GRANT SELECT ON [dbo].[ClinicsMessagesRules] TO [bliddle] AS [dbo]
+GO
+GRANT UPDATE ON [dbo].[ClinicsMessagesRules] TO [bliddle] AS [dbo]
+GO
+GRANT INSERT ON [dbo].[ClinicsMessagesRules] TO [cgulley] AS [dbo]
+GO
+GRANT SELECT ON [dbo].[ClinicsMessagesRules] TO [cgulley] AS [dbo]
+GO
+GRANT UPDATE ON [dbo].[ClinicsMessagesRules] TO [cgulley] AS [dbo]
+GO
+GRANT INSERT ON [dbo].[ClinicsMessagesRules] TO [dhill] AS [dbo]
+GO
+GRANT SELECT ON [dbo].[ClinicsMessagesRules] TO [dhill] AS [dbo]
+GO
+GRANT UPDATE ON [dbo].[ClinicsMessagesRules] TO [dhill] AS [dbo]
+GO
+GRANT INSERT ON [dbo].[ClinicsMessagesRules] TO [ezobel] AS [dbo]
+GO
+GRANT SELECT ON [dbo].[ClinicsMessagesRules] TO [ezobel] AS [dbo]
+GO
+GRANT UPDATE ON [dbo].[ClinicsMessagesRules] TO [ezobel] AS [dbo]
+GO
+GRANT INSERT ON [dbo].[ClinicsMessagesRules] TO [jsteidinger] AS [dbo]
+GO
+GRANT TAKE OWNERSHIP ON [dbo].[ClinicsMessagesRules] TO [jsteidinger] AS [dbo]
+GO
+GRANT INSERT ON [dbo].[ClinicsMessagesRules] TO [mmoscoso] AS [dbo]
+GO
+GRANT SELECT ON [dbo].[ClinicsMessagesRules] TO [mmoscoso] AS [dbo]
+GO
+GRANT UPDATE ON [dbo].[ClinicsMessagesRules] TO [mmoscoso] AS [dbo]
+GO
+GRANT INSERT ON [dbo].[ClinicsMessagesRules] TO [nwest] AS [dbo]
+GO
+GRANT SELECT ON [dbo].[ClinicsMessagesRules] TO [nwest] AS [dbo]
+GO
+GRANT UPDATE ON [dbo].[ClinicsMessagesRules] TO [nwest] AS [dbo]
+GO
+GRANT INSERT ON [dbo].[ClinicsMessagesRules] TO [rspears] AS [dbo]
+GO
+GRANT SELECT ON [dbo].[ClinicsMessagesRules] TO [rspears] AS [dbo]
+GO
+GRANT UPDATE ON [dbo].[ClinicsMessagesRules] TO [rspears] AS [dbo]
+GO
+GRANT INSERT ON [dbo].[ClinicsMessagesRules] TO [rwilson] AS [dbo]
+GO
+GRANT SELECT ON [dbo].[ClinicsMessagesRules] TO [rwilson] AS [dbo]
+GO
+GRANT UPDATE ON [dbo].[ClinicsMessagesRules] TO [rwilson] AS [dbo]
 GO
