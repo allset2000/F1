ed8cb760482866dd256d09ff1222916202a269bc
DDorsey <ddorsey@entradahealth.com>
2015-04-15 10:54:06 -0500

    Missing index change in CC2

--- HostedClient/Tables/dbo.Users.sql
+++ HostedClient/Tables/dbo.Users.sql
@@ -7,10 +7,10 @@ CREATE TABLE [dbo].[Users]
 [Password] [varchar] (64) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [Salt] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY]
+CREATE NONCLUSTERED INDEX [IX_Users_LoginEmail] ON [dbo].[Users] ([LoginEmail]) ON [PRIMARY]
+
 GO
 ALTER TABLE [dbo].[Users] ADD CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED  ([UserID]) ON [PRIMARY]
 GO
 CREATE NONCLUSTERED INDEX [IX_Users_ClinicID] ON [dbo].[Users] ([ClinicID]) ON [PRIMARY]
-GO
-CREATE UNIQUE NONCLUSTERED INDEX [IX_Users_LoginEmail] ON [dbo].[Users] ([LoginEmail]) ON [PRIMARY]
 GO
