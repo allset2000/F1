46d71f52fe210815e04b753ffc6b05535143b900
DDorsey <ddorsey@entradahealth.com>
2015-04-28 10:50:00 -0500

    Index creation on SchedulesTracking

--- HostedClient/Tables/dbo.SchedulesTracking.sql
+++ HostedClient/Tables/dbo.SchedulesTracking.sql
@@ -24,6 +24,8 @@ CREATE TABLE [dbo].[SchedulesTracking]
 [AttendingLast] [varchar] (120) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [Type] [varchar] (1) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF__SchedulesT__Type__5540965B] DEFAULT ('S')
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+CREATE NONCLUSTERED INDEX [IX_SchedulesTracking_ChangedOn] ON [dbo].[SchedulesTracking] ([ChangedOn] DESC) ON [PRIMARY]
+
 GO
 ALTER TABLE [dbo].[SchedulesTracking] ADD CONSTRAINT [PK_EncountersTracking] PRIMARY KEY CLUSTERED  ([SchedulesTrackingID]) ON [PRIMARY]
 GO
