37c01ea081633ca55f93fde36a84444266074022
MBayramov <mbayramov@entradahealth.com>
2015-05-04 15:25:28 -0500

    Bug #4327: Problem with Data Archiving

--- EntradaArchive/Tables/dbo.Jobs_Documents_History.sql
+++ EntradaArchive/Tables/dbo.Jobs_Documents_History.sql
@@ -11,10 +11,13 @@ CREATE TABLE [dbo].[Jobs_Documents_History]
 [DocumentStatusId] [int] NOT NULL CONSTRAINT [DF_Jobs_Documents_History_DocumentStatusId] DEFAULT ((0)),
 [JobId] [int] NOT NULL CONSTRAINT [DF_Jobs_Documents_History_JobId] DEFAULT ((0)),
 [TemplateName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Status] [smallint] NULL,
+[StatusDate] [datetime] NULL,
 [ArchiveID] [int] NOT NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+ALTER TABLE [dbo].[Jobs_Documents_History] ADD 
+CONSTRAINT [PK_Jobs_Documents_History] PRIMARY KEY CLUSTERED  ([DocumentID]) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[Jobs_Documents_History] ADD CONSTRAINT [PK_Jobs_Documents_History] PRIMARY KEY CLUSTERED  ([DocumentID]) ON [PRIMARY]
-GO
+
 ALTER TABLE [dbo].[Jobs_Documents_History] ADD CONSTRAINT [FK_Jobs_Documents_History_ArchiveLog] FOREIGN KEY ([ArchiveID]) REFERENCES [dbo].[ArchiveLog] ([ArchiveID])
 GO
