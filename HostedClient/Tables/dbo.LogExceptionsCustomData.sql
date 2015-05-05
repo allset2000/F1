2234a7f42be940bbfee770709f3b6341b348d21b
MBayramov <mbayramov@entradahealth.com>
2015-05-05 08:35:11 -0500

    #3889# Added ApiKey and DeliveryID

--- HostedClient/Tables/dbo.LogExceptionsCustomData.sql
+++ HostedClient/Tables/dbo.LogExceptionsCustomData.sql
@@ -30,7 +30,9 @@ CREATE TABLE [dbo].[LogExceptionsCustomData]
 [MobileOS] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [QuickBloxUserId] [int] NULL,
 [JobTypeID] [int] NULL,
-[UserAgent] [varchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[UserAgent] [varchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ApiKey] [varchar] (300) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[DeliveryID] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[LogExceptionsCustomData] ADD CONSTRAINT [PK_LogExceptionsCustomData] PRIMARY KEY CLUSTERED  ([LogExceptionsCustomDataID]) ON [PRIMARY]
