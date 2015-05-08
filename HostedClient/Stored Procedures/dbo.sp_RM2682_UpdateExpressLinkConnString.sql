66db0d37910fa474880a63a43f62780ed4fdaf71
DDorsey <ddorsey@entradahealth.com>
2015-05-08 09:36:38 -0500

    #2682# - Update to temp stored proc

--- HostedClient/Stored Procedures/dbo.sp_RM2682_UpdateExpressLinkConnString.sql
+++ HostedClient/Stored Procedures/dbo.sp_RM2682_UpdateExpressLinkConnString.sql
@@ -1,28 +1,47 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
 
-
 -- =============================================
 -- Author: Dustin Dorsey
 -- Create date: 4/30/15
--- Description: Temp SP used to update connectionstring in ExpressLinkConfiguration Table
+-- Description: Temp SP used to update values in ExpressLinkConfiguration Table
 -- =============================================
 
 CREATE PROCEDURE [dbo].[sp_RM2682_UpdateExpressLinkConnString] 
 (
 @ID int,
-@ConnectionString Varchar(MAX)
+@ConnectionString Varchar(MAX) = NULL,
+@DaysForward smallint = NULL,
+@DaysBack smallint = NULL,
+@LastScheduleSync datetime = NULL
 ) 
 
 AS 
 
 BEGIN
 	
-update expressLinkConfigurations 
-set connectionstring = @ConnectionString
-where ID = @ID 
+	IF (@ConnectionString is not null)
+	BEGIN
+		UPDATE expressLinkConfigurations SET ConnectionString=@ConnectionString where ID = @ID 
+	END
+
+	IF (@DaysForward is not null)
+	BEGIN
+		UPDATE expressLinkConfigurations SET DaysForward = @DaysForward where ID = @ID 
+	END
+
+	IF (@DaysBack is not null)
+	BEGIN
+		UPDATE expressLinkConfigurations SET DaysBack = @DaysBack where ID = @ID 
+	END
+
+	IF (@LastScheduleSync is not null)
+	BEGIN
+		UPDATE expressLinkConfigurations SET LastScheduleSync = @LastScheduleSync where ID = @ID 
+	END
 	
 END
 
