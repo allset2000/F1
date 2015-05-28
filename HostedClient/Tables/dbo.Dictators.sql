2a902e24e9d9157a3286da26c6093677e1cc0717
DDorsey <ddorsey@entradahealth.com>
2015-05-28 11:24:30 -0500

    Missing Indexes in source control

--- HostedClient/Tables/dbo.Dictators.sql
+++ HostedClient/Tables/dbo.Dictators.sql
@@ -1,39 +1,40 @@
-CREATE TABLE [dbo].[Dictators](
-	[DictatorID] [int] IDENTITY(1,1) NOT NULL,
-	[DictatorName] [varchar](50) NOT NULL CONSTRAINT [DF_Dictators_DictatorName]  DEFAULT (''),
-	[ClinicID] [smallint] NOT NULL,
-	[Deleted] [bit] NOT NULL CONSTRAINT [DF_Dictators_Active]  DEFAULT ((0)),
-	[DefaultJobTypeID] [int] NOT NULL,
-	[Password] [varchar](64) NOT NULL,
-	[Salt] [varchar](32) NOT NULL,
-	[FirstName] [varchar](100) NOT NULL CONSTRAINT [DF_Dictators_FirstName]  DEFAULT (''),
-	[MI] [varchar](100) NOT NULL CONSTRAINT [DF_Dictators_MI]  DEFAULT (''),
-	[LastName] [varchar](100) NOT NULL CONSTRAINT [DF_Dictators_LastName]  DEFAULT (''),
-	[Suffix] [varchar](100) NOT NULL CONSTRAINT [DF_Dictators_Suffix]  DEFAULT (''),
-	[Initials] [varchar](20) NOT NULL CONSTRAINT [DF_Dictators_Initials]  DEFAULT (''),
-	[Signature] [varchar](1000) NOT NULL CONSTRAINT [DF_Dictators_Signature]  DEFAULT (''),
-	[EHRProviderID] [varchar](36) NOT NULL,
-	[EHRProviderAlias] [varchar](36) NOT NULL,
-	[DefaultQueueID] [int] NULL,
-	[VRMode] [smallint] NOT NULL CONSTRAINT [DF__Dictators__VRMod__4E1E9780]  DEFAULT ((99)),
-	[CRFlagType] [int] NOT NULL CONSTRAINT [DF_Dictators_CRFlagType]  DEFAULT ((0)),
-	[ForceCRStartDate] [datetime] NULL,
-	[ForceCREndDate] [datetime] NULL,
-	[ExcludeStat] [bit] NOT NULL CONSTRAINT [DF_Dictators_ExcludeStat]  DEFAULT ((0)),
-	[SignatureImage] [varbinary](max) NULL,
-	[ImageName] [varchar](100) NULL,
-	[UserId] [int] NULL,
-	[SRETypeId] [int] NULL,
- CONSTRAINT [PK_Dictators] PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Dictators]
 (
-	[DictatorID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
- CONSTRAINT [IX_Dictators_Unique_DictatorName] UNIQUE NONCLUSTERED 
-(
-	[DictatorName] ASC,
-	[ClinicID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+[DictatorID] [int] NOT NULL IDENTITY(1, 1),
+[DictatorName] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Dictators_DictatorName] DEFAULT (''),
+[ClinicID] [smallint] NOT NULL,
+[Deleted] [bit] NOT NULL CONSTRAINT [DF_Dictators_Active] DEFAULT ((0)),
+[DefaultJobTypeID] [int] NOT NULL,
+[Password] [varchar] (64) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Salt] [varchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[FirstName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Dictators_FirstName] DEFAULT (''),
+[MI] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Dictators_MI] DEFAULT (''),
+[LastName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Dictators_LastName] DEFAULT (''),
+[Suffix] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Dictators_Suffix] DEFAULT (''),
+[Initials] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Dictators_Initials] DEFAULT (''),
+[Signature] [varchar] (1000) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL CONSTRAINT [DF_Dictators_Signature] DEFAULT (''),
+[EHRProviderID] [varchar] (36) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[EHRProviderAlias] [varchar] (36) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[DefaultQueueID] [int] NULL,
+[VRMode] [smallint] NOT NULL CONSTRAINT [DF__Dictators__VRMod__4E1E9780] DEFAULT ((99)),
+[CRFlagType] [int] NOT NULL CONSTRAINT [DF_Dictators_CRFlagType] DEFAULT ((0)),
+[ForceCRStartDate] [datetime] NULL,
+[ForceCREndDate] [datetime] NULL,
+[ExcludeStat] [bit] NOT NULL CONSTRAINT [DF_Dictators_ExcludeStat] DEFAULT ((0)),
+[SignatureImage] [varbinary] (max) NULL,
+[ImageName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[UserId] [int] NULL,
+[SRETypeId] [int] NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+ALTER TABLE [dbo].[Dictators] ADD 
+CONSTRAINT [PK_Dictators] PRIMARY KEY CLUSTERED  ([DictatorID]) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Dictators] ADD CONSTRAINT [IX_Dictators_Unique_DictatorName] UNIQUE NONCLUSTERED  ([DictatorName], [ClinicID]) ON [PRIMARY]
+
+CREATE NONCLUSTERED INDEX [IX_Dictators_ClinicID] ON [dbo].[Dictators] ([ClinicID], [EHRProviderID]) ON [PRIMARY]
+
+CREATE NONCLUSTERED INDEX [IX_Dictators_DictatorName] ON [dbo].[Dictators] ([DictatorName]) ON [PRIMARY]
+
 
 GO
 
@@ -76,5 +77,3 @@ GO
 
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Default Job Type' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Dictators', @level2type=N'COLUMN',@level2name=N'DefaultJobTypeID'
 GO
-
-
