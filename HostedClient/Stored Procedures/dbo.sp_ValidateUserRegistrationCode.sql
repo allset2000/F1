22076ce65ff44f3d4e056167fa2e0305966e84bf
Samuel Shoultz <sshoultz@entradahealth.com>
2015-06-05 08:05:56 -0500

    #4591# - Updated WS validation SP to honor deleted flag

--- HostedClient/Stored Procedures/dbo.sp_ValidateUserRegistrationCode.sql
+++ HostedClient/Stored Procedures/dbo.sp_ValidateUserRegistrationCode.sql
@@ -9,6 +9,11 @@ GO
 -- Author: Sam Shoultz
 -- Create date: 1/20/2015
 -- Description: SP Used to validate the Registration Code sent in from mobile
+
+-- Modified By: Sam Shoultz
+-- Modified On: 6/5/2015
+-- Release: D.2
+-- Modifications: Added UI.Deleted = 0 on where clause (introduced deleting invitations in this release)
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_ValidateUserRegistrationCode] (
 	@RegCode varchar(20)
@@ -23,6 +28,7 @@ BEGIN
 		LEFT OUTER JOIN Clinics C on C.ClinicId = UI.ClinicId
 	WHERE SUBSTRING(SecurityToken, 0, CHARINDEX('-', SecurityToken, 0)) = @ShortCode
 	and UI.RegisteredUserId is null
+	and UI.Deleted = 0
 
 END
 
