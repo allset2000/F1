a04ebe792b6e109e9810e21bc86ce3e01e107ff4
DDorsey <ddorsey@entradahealth.com>
2015-06-15 12:52:30 -0500

    #4176# Renaming temp stored proc

--- Backend/Stored Procedures/dbo.sp_RM4716_DocumentsToProcessErrorCountReset.sql
+++ Backend/Stored Procedures/dbo.sp_RM4716_DocumentsToProcessErrorCountReset.sql
@@ -2,23 +2,18 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-
 -- =============================================
 -- Author: Dustin Dorsey
 -- Create date: 6/15/2015
 -- Description: Temp SP used to set Failure Count in DocumentsToProcess back to 0 for all jobs that have an error count of 5 
 -- =============================================
-
-CREATE PROCEDURE [dbo].[sp_4716_DocumentsToProcessErrorCountReset] 
-
-AS 
-
+CREATE PROCEDURE [dbo].[sp_RM4716_DocumentsToProcessErrorCountReset] 
+AS
 BEGIN
-	
+       
   UPDATE [Entrada].[dbo].[DocumentsToProcess]
   SET [ProcessFailureCount] = 0
   WHERE [ProcessFailureCount] = 5
-	
+       
 END
-
 GO
