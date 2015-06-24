08ddd8fdfbf0474d404157ab60c162e27efe6ded
Samuel Shoultz <sshoultz@entradahealth.com>
2015-06-24 07:34:20 -0500

    #4355# - Timezone table change (added GMTOffset column)

--- HostedClient/Tables/dbo.TimeZone.sql
+++ HostedClient/Tables/dbo.TimeZone.sql
@@ -1,8 +1,9 @@
 CREATE TABLE [dbo].[TimeZone]
 (
 [TimeZoneId] [int] NOT NULL IDENTITY(1, 1),
-[TimeZoneName] [varchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+[TimeZoneName] [varchar] (3) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[GMTOffset] [int] NOT NULL
 ) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[TimeZone] ADD CONSTRAINT [PK_TimeZone] PRIMARY KEY CLUSTERED  ([TimeZoneId]) ON [PRIMARY]
+ALTER TABLE [dbo].[TimeZone] ADD 
+CONSTRAINT [PK_TimeZone] PRIMARY KEY CLUSTERED  ([TimeZoneId]) ON [PRIMARY]
 GO
