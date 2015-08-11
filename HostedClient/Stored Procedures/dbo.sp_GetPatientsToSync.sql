5e095713f230879e31bed7ab7cea1681097d853c
Samuel Shoultz <sshoultz@entradahealth.com>
2015-08-11 13:59:44 -0500

    #000# - Added missing data on the patients object for mobile

--- HostedClient/Stored Procedures/dbo.sp_GetPatientsToSync.sql
+++ HostedClient/Stored Procedures/dbo.sp_GetPatientsToSync.sql
@@ -12,13 +12,17 @@ GO
 --	Modified By: Mikayil Bayramov
 --	Modification Date: 5/29/2015
 --	Details: Performance improvement.
+
+--  Modified By: Sam Shoultz
+--  Modification Date: 8/11/2015
+--  Details: Added p.Gender, Address1, Address2, City, State, Zip - these values are needed by mobile
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_GetPatientsToSync] (
 	 @DictatorId INT,
 	 @MaxFutureDays INT
 ) AS 
 BEGIN
-	SELECT DISTINCT p.PatientID, p.MRN, p.AlternateID, p.ClinicID, p.DOB, p.FirstName, p.MI, p.LastName, p.Suffix, p.PrimaryCareProviderID
+	SELECT DISTINCT p.PatientID, p.MRN, p.AlternateID, p.ClinicID, p.DOB, p.FirstName, p.MI, p.LastName, p.Suffix, p.PrimaryCareProviderID, p.Gender, p.Address1, p.Address2, p.City, p.State, p.Zip
 	FROM dbo.Patients AS p INNER JOIN dbo.Encounters AS e ON p.PatientID = e.PatientID
 						   INNER JOIN dbo.Jobs AS j ON e.EncounterID = j.EncounterID
 						   INNER JOIN dbo.Dictations AS d ON d.JobID = j.JobID 
