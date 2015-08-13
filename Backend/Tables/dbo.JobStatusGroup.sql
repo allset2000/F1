318a55a7cc833d3e56b678fdaea8fdefc9c1310e
rnarender <ramadheni.narender@adroitent.com>
2015-08-14 02:52:55 +0530

    #0000# There were of D.2 item, got checked in D.1, so removing from D.1

--- Backend/Tables/dbo.JobStatusGroup.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[JobStatusGroup]
-(
-[Id] [int] NOT NULL IDENTITY(1, 1),
-[StatusGroup] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
-) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[JobStatusGroup] ADD CONSTRAINT [PK_JobStatusOptions] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
-GO
