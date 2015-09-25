0af39eb10dc2d517d60c99940a51623bd1fd46a8
MBayramov <mbayramov@entradahealth.com>
2015-09-25 14:37:01 -0500

    see Modification Code: #001 in comments

--- HostedClient/Stored Procedures/dbo.sp_CreateUserInvitation.sql
+++ HostedClient/Stored Procedures/dbo.sp_CreateUserInvitation.sql
@@ -3,10 +3,17 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
+
 -- =============================================
 -- Author: Santhosh Mukk
 -- Create date: 01/14/2015
 -- Description: SP used to Create new Invitation
+
+
+-- Modified By: Mikayil Bayramov
+-- Modified On: 9/25/2015
+-- Modification Details: Added validation for User Role(s) and Clinic
+-- Modification Code: #001
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_CreateUserInvitation]
 (
@@ -25,37 +32,69 @@ CREATE PROCEDURE [dbo].[sp_CreateUserInvitation]
 )
 AS
 BEGIN
-INSERT INTO [dbo].[UserInvitations]
-           ([FirstName]
-           ,[MI]
-           ,[LastName]
-           ,[EmailAddress]
-           ,[PhoneNumber]
-           ,[InvitationSent]
-           ,[ClinicId]
-           ,[InvitationMethod]
-           ,[RoleId]
-           ,[SecurityToken]
-           ,[DateTimeInvitationSent]
-           ,[InvitationTypeId]
-		   ,[RequestingUserId]
-		   ,[DateTimeRequested]
-		   ,[InvitationMessage])
-     VALUES
-           (@FirstName
-           ,@MI
-           ,@LastName
-           ,@EmailAddress
-           ,@PhoneNumber
-           ,0
-           ,@ClinicId
-           ,@InvitationMethod
-           ,@RoleId
-           ,@SecurityToken
-           ,GETDATE()
-           ,@InvitationTypeId
-		   ,@RequestingUserId
-		   ,GETDATE()
-		   ,@InvitationMessage)
+	/*
+	--Modification: #001
+	If role id is:
+		1) negative number or zero (this value can be set from Dictate API),
+		2) is null
+		3) empty
+	then it is interpreted as no role.
+	In this case get the default role RUDefualtRole.
+	*/
+	IF(@RoleId <= 0 OR @RoleId IS NULL OR LEN(LTRIM(RTRIM(@RoleId))) <= 0) BEGIN
+		IF EXISTS(select 1 from SystemConfiguration where ConfigKey = 'RUDefualtRole') BEGIN
+			SET @RoleId = (select ConfigValue from SystemConfiguration where ConfigKey = 'RUDefualtRole')
+		END
+		ELSE BEGIN
+			RAISERROR ('Server Configuration not setup', 16, 1);
+		END
+	END
+
+	/*
+	--Modification: #001
+	If Clinic Id is less than zero (negative number) or is null, 
+	then get the default clinic id from SMDefaultClinic
+	*/
+	IF(@ClinicId < 0 OR @ClinicId IS NULL) BEGIN
+		IF EXISTS(select 1 from SystemConfiguration where ConfigKey = 'SMDefaultClinic') BEGIN
+			SET @ClinicId = (select ConfigValue from SystemConfiguration where ConfigKey = 'SMDefaultClinic')
+		END
+		ELSE BEGIN
+			RAISERROR ('Server Configuration not setup',16,1);
+		END
+	END
+
+	INSERT INTO [dbo].[UserInvitations]
+			   ([FirstName]
+			   ,[MI]
+			   ,[LastName]
+			   ,[EmailAddress]
+			   ,[PhoneNumber]
+			   ,[InvitationSent]
+			   ,[ClinicId]
+			   ,[InvitationMethod]
+			   ,[RoleId]
+			   ,[SecurityToken]
+			   ,[DateTimeInvitationSent]
+			   ,[InvitationTypeId]
+			   ,[RequestingUserId]
+			   ,[DateTimeRequested]
+			   ,[InvitationMessage])
+		 VALUES
+			   (@FirstName
+			   ,@MI
+			   ,@LastName
+			   ,@EmailAddress
+			   ,@PhoneNumber
+			   ,0
+			   ,@ClinicId
+			   ,@InvitationMethod
+			   ,@RoleId
+			   ,@SecurityToken
+			   ,GETDATE()
+			   ,@InvitationTypeId
+			   ,@RequestingUserId
+			   ,GETDATE()
+			   ,@InvitationMessage)
 END
 GO
