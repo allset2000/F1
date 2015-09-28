8e687abb5e04ad213567beb33687edbaea6d3170
MBayramov <mbayramov@entradahealth.com>
2015-09-28 13:01:08 -0500

    #000 Implemented new improved logic to validate new invitations

--- HostedClient/Stored Procedures/dbo.sp_ValidateNewInvitation.sql
+++ HostedClient/Stored Procedures/dbo.sp_ValidateNewInvitation.sql
@@ -9,6 +9,11 @@ GO
 -- Author: Sam Shoultz
 -- Create date: 05/04/2015
 -- Description: SP used to validate and return a user invitation if one already exists
+
+-- Modified by: Mikayil Bayramov	
+-- Modified Date: 9/28/2015
+-- Modification Code: 001
+-- Modification Details: Implemented new improvced logic to validate new invitations
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_ValidateNewInvitation]
 (
@@ -18,9 +23,16 @@ CREATE PROCEDURE [dbo].[sp_ValidateNewInvitation]
 )
 AS
 BEGIN
+	SELECT TOP 1 UserInvitationId, FirstName, MI, LastName, EmailAddress, PhoneNumber, InvitationSent, RequestingUserId, ClinicId,
+		         InvitationMethod, RoleId, SecurityToken, DateTimeRequested, DateTimeInvitationSent, IsDemoUSer, RegisteredUserId, InvitationMessage
+	FROM UserInvitations
+	WHERE RegisteredUserId IS NULL AND 
+	     PhoneNumber = COALESCE(@PhoneNumber, PhoneNumber) AND
+		 EmailAddress = COALESCE(@EmailAddress, EmailAddress)
 
-	IF EXISTS (SELECT 1 FROM UserInvitations WHERE RegisteredUserId is null and (PhoneNumber = @PhoneNumber or EmailAddress = @EmailAddress))
-	BEGIN
+	/*
+	Diabled in favour of new improved implementation. See Modification code: 001
+	IF EXISTS (SELECT 1 FROM UserInvitations WHERE RegisteredUserId is null and (PhoneNumber = @PhoneNumber or EmailAddress = @EmailAddress)) BEGIN
 		SELECT TOP 1 UserInvitationId,
 					 FirstName,
 					 MI,
@@ -41,7 +53,7 @@ BEGIN
 		FROM UserInvitations
 		WHERE RegisteredUserId is null and (PhoneNumber = @PhoneNumber or EmailAddress = @EmailAddress)
 	END
-
+	*/
 END
 
 
