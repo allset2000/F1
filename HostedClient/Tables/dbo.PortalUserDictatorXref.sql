96220ac257283910a2a5b33f59b27525920eb38e
Vivek Narayanaswamy <vivekn@entradahealth.com>
2015-09-28 01:23:40 -0500

    added FKs to UserId, dictatorId fields.

--- HostedClient/Tables/dbo.PortalUserDictatorXref.sql
+++ HostedClient/Tables/dbo.PortalUserDictatorXref.sql
@@ -5,6 +5,10 @@ CREATE TABLE [dbo].[PortalUserDictatorXref]
 [DictatorId] [int] NOT NULL,
 [IsDeleted] [bit] NOT NULL CONSTRAINT [DF_PortalUserDictatorXref_IsDeleted] DEFAULT ((0))
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[PortalUserDictatorXref] ADD
+CONSTRAINT [FK_PortalUserDictatorXref_Dictators] FOREIGN KEY ([DictatorId]) REFERENCES [dbo].[Dictators] ([DictatorID])
+ALTER TABLE [dbo].[PortalUserDictatorXref] ADD
+CONSTRAINT [FK_PortalUserDictatorXref_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([UserID])
 GO
 ALTER TABLE [dbo].[PortalUserDictatorXref] ADD CONSTRAINT [PK_PortalUserDictatorXref] PRIMARY KEY CLUSTERED  ([PortalUserDictatorXrefId]) ON [PRIMARY]
 GO
