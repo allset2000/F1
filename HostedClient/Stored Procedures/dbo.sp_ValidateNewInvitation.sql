bb9d090766abc006659b0731e90c5fde393356c6
MBayramov <mbayramov@entradahealth.com>
2015-09-29 09:14:21 -0500

    #000 Removed commented code from [dbo].[sp_ValidateNewInvitation]

--- HostedClient/Stored Procedures/dbo.sp_ValidateNewInvitation.sql
+++ HostedClient/Stored Procedures/dbo.sp_ValidateNewInvitation.sql
@@ -12,7 +12,6 @@ GO
 
 -- Modified by: Mikayil Bayramov	
 -- Modified Date: 9/28/2015
--- Modification Code: 001
 -- Modification Details: Implemented new improvced logic to validate new invitations
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_ValidateNewInvitation]
@@ -29,31 +28,6 @@ BEGIN
 	WHERE RegisteredUserId IS NULL AND 
 	     PhoneNumber = COALESCE(@PhoneNumber, PhoneNumber) AND
 		 EmailAddress = COALESCE(@EmailAddress, EmailAddress)
-
-	/*
-	Diabled in favour of new improved implementation. See Modification code: 001
-	IF EXISTS (SELECT 1 FROM UserInvitations WHERE RegisteredUserId is null and (PhoneNumber = @PhoneNumber or EmailAddress = @EmailAddress)) BEGIN
-		SELECT TOP 1 UserInvitationId,
-					 FirstName,
-					 MI,
-					 LastName,
-					 EmailAddress,
-					 PhoneNumber,
-					 InvitationSent,
-					 RequestingUserId,
-					 ClinicId,
-					 InvitationMethod,
-					 RoleId,
-					 SecurityToken,
-					 DateTimeRequested,
-					 DateTimeInvitationSent,
-					 IsDemoUSer,
-					 RegisteredUserId,
-					 InvitationMessage
-		FROM UserInvitations
-		WHERE RegisteredUserId is null and (PhoneNumber = @PhoneNumber or EmailAddress = @EmailAddress)
-	END
-	*/
 END
 
 
