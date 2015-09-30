4686f481d8948f92fb0b2d9628156f903677c496
Vivek Narayanaswamy <vivekn@entradahealth.com>
2015-09-29 19:36:55 -0500

    Fk on variabletypeid. Removed VariableTypes table since RowVariableTypes table exists.

--- HostedClient/Tables/dbo.VariableTypes.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[VariableTypes]
-(
-[VariableTypeId] [int] NOT NULL IDENTITY(1, 1),
-[Description] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
-) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[VariableTypes] ADD CONSTRAINT [PK_VariableTypes] PRIMARY KEY CLUSTERED  ([VariableTypeId]) ON [PRIMARY]
-GO
