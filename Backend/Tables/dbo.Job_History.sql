23cd18034695243ee78b98c2371a8c5ea6b82a8d
DDorsey <ddorsey@entradahealth.com>
2015-10-01 15:19:52 -0500

    #000# Updated Constraint names and added indexes on FK

--- Backend/Tables/dbo.Job_History.sql
+++ Backend/Tables/dbo.Job_History.sql
@@ -1,30 +1,26 @@
-CREATE TABLE [dbo].[Job_History](
-	[JobHistoryID] [int] IDENTITY(1,1) NOT NULL,
-	[JobNumber] [varchar](20) NOT NULL,
-	[MRN] INT NULL,
-	[JobType] [varchar](100) NULL,
-	[CurrentStatus] [smallint] NOT NULL,
-	[DocumentID] [int] NULL,
-	[UserId] [varchar](48) NULL,
-	[HistoryDateTime] [datetime] NOT NULL,
-PRIMARY KEY CLUSTERED 
+CREATE TABLE [dbo].[Job_History]
 (
-	[JobHistoryID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+[JobHistoryID] [int] NOT NULL IDENTITY(1, 1),
+[JobNumber] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[MRN] [int] NULL,
+[JobType] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CurrentStatus] [smallint] NOT NULL,
+[DocumentID] [int] NULL,
+[UserId] [varchar] (48) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[HistoryDateTime] [datetime] NOT NULL
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[Job_History] ADD 
+CONSTRAINT [PK_Job_History] PRIMARY KEY CLUSTERED  ([JobHistoryID]) ON [PRIMARY]
+CREATE NONCLUSTERED INDEX [IX_Job_History_DocumentId] ON [dbo].[Job_History] ([DocumentID]) ON [PRIMARY]
+
+CREATE NONCLUSTERED INDEX [IX_Job_History_JobNumber] ON [dbo].[Job_History] ([JobNumber]) ON [PRIMARY]
+
+ALTER TABLE [dbo].[Job_History] ADD
+CONSTRAINT [FK_Job_History_DocumentID] FOREIGN KEY ([DocumentID]) REFERENCES [dbo].[Jobs_Documents_History] ([DocumentID])
+ALTER TABLE [dbo].[Job_History] ADD
+CONSTRAINT [FK_Job_History_JobNumber] FOREIGN KEY ([JobNumber]) REFERENCES [dbo].[Jobs] ([JobNumber])
 
 GO
 
 SET ANSI_PADDING OFF
 GO
-
-ALTER TABLE [dbo].[Job_History]  WITH CHECK ADD FOREIGN KEY([DocumentID])
-REFERENCES [dbo].[Jobs_Documents_History] ([DocumentID])
-GO
-
-ALTER TABLE [dbo].[Job_History]  WITH CHECK ADD FOREIGN KEY([JobNumber])
-REFERENCES [dbo].[Jobs] ([JobNumber])
-GO
-
-
-
