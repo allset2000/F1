666aa13ccb902abc01587ce62296a781919b3ca4
Vivek Narayanaswamy <vivekn@entradahealth.com>
2015-10-09 15:51:12 -0500

    #3299# - Mike changed this.

--- HostedClient/Stored Procedures/dbo.ins_upd_schedule.sql
+++ HostedClient/Stored Procedures/dbo.ins_upd_schedule.sql
@@ -59,7 +59,7 @@ END
 
 IF ISNULL(@additionalData,'')=''
 BEGIN
-	SET @additionalData = '<custom1>'+@encounterID+'</custom1><custom5>'+@locationName+'</custom5>'
+	SET @additionalData = '<custom1>'+@encounterID+'</custom1><custom5>'+@locationName+'</custom5><custom6>'+@LocationID+'</custom6><custom10>'+@ReferringID+'</custom10><custom11>'+@referringName+'</custom11>'
 END
 
 -- If the EHR isn't providing us the attending first/last name, pull it from the Dictators table
