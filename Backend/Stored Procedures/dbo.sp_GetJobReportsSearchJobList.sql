2f2285f229e2a3f7ab39a406605ce4302309d819
srachabandi <suresh.rachabandi@adroitent.com>
2015-10-13 18:30:32 +0530

    #3163# is completed

--- Backend/Stored Procedures/dbo.sp_GetJobReportsSearchJobList.sql
+++ Backend/Stored Procedures/dbo.sp_GetJobReportsSearchJobList.sql
@@ -46,67 +46,87 @@ BEGIN
 		If(@IsSaved is NULL) 
 			return -1 -- this condition is to make sure that we get a valid filter record, not to try to get a record which was deleted
 
+			if @DateField is null 
+				set @DateField=1 -- this condition is to get primary group status, is @DateField is null 
+
+			if @DateField = 5 
+				set @JobStatus=5 -- this condition is to sure jobs are available in JobDeliveryHistory, don't consider 360 status
+
 	IF OBJECT_ID('tempdb..#SearchItems') IS NOT NULL
     DROP TABLE #SearchItems
+
+	DECLARE @JobsToFilter TABLE(  JobNumber VARCHAR(20), StatusDate datetime, ClinicID int) 
 
 	CREATE TABLE #SearchItems (
 	Jobnumber varchar(20) NULL, DictatorID varchar(50) NULL, JobType varchar(100) NULL, 
 	IsGenericJob bit NULL, AppointmentDate smalldatetime NULL, CC bit null, Stat bit NULL, MRN varchar(50), 
 	Patient varchar(120) NULL, FirstName varchar(50) NULL, LastName varchar(50) NULL, JobStatus varchar(120) NULL, InProcess Datetime null, AwaitingDelivery datetime NULL)
 
+
+	
+	INSERT INTO @JobsToFilter 
+					SELECT JT.JobNumber, MAX(jt.StatusDate) StatusDate,j.ClinicID
+						FROM dbo.JobTracking JT
+							INNER JOIN dbo.StatusCodes SC on JT.Status= SC.StatusID
+							INNER JOIN dbo.JobStatusGroup JG on JG.Id = SC.StatusGroupId
+							INNER JOIN jobs j on j.jobnumber = jt.jobnumber 
+						WHERE	(@DateField is null or JG.Id= @DateField)
+							and (@From is null or JT.StatusDate  >= @From)
+							and (@To is null or JT.StatusDate  <= @To) 
+							and (@RangeOptionFromDate is null or JT.StatusDate  >= @RangeOptionFromDate) 
+							and (@RangeOptionToDate is null or JT.StatusDate  <= @RangeOptionToDate) 
+							and (@JobType is null or J.JobType = @JobType) 
+							and (@DictatorID is null or J.DictatorID = @DictatorID) 
+							and (@DeviceGenerated is null or J.IsGenericJob = @DeviceGenerated) 
+							and (@CC is null or J.CC = @CC) 
+							and (@STAT is null or J.Stat = @STAT) 
+							and (@ClinicID is null or J.ClinicID = @ClinicID)  
+							and (@JobNumber is null or J.JobNumber = @JobNumber) 
+							--and JT.StatusDate >= DATEADD(M,-3,getdate()) -- will  always get lessthan 3 months data..
+							and (J.ReceivedOn  >= DATEADD(M,-3,getdate()))
+						GROUP BY jg.Id,JG.StatusGroup, JT.JobNumber,j.ClinicID 
+
+
 	INSERT INTO #SearchItems 
 	SELECT J.JobNumber, J.DictatorID, J.JobType, J.IsGenericJob as DeviceGenerated,J.AppointmentDate,J.CC,J.Stat, JP.MRN, (JP.FirstName + ' '+ JP.LastName) AS Patient, 
-	JP.FirstName, JP.LastName, CASE WHEN JD.ID=5 THEN JD.JobDeliveredOn ELSE js.JobStatus end JobStatus, JTI.StatusDate AS 'InProcess', jta.StatusDate AS 'AwaitingDelevery'
-		FROM	dbo.Jobs J 
-		INNER JOIN dbo.Jobs_Patients JP ON J.JobNumber = JP.JobNumber 
-		INNER JOIN dbo.Dictators D ON J.DictatorID = D.DictatorID
-			OUTER APPLY(SELECT JT.JobNumber,MAX(jt.StatusDate) StatusDate,jg.Id,JG.StatusGroup, @GroupBy as selectedGrpBy
-			   FROM dbo.JobTracking JT  
-			   INNER JOIN dbo.StatusCodes SC on JT.Status= SC.StatusID
-			   INNER JOIN dbo.JobStatusGroup JG on JG.Id = SC.StatusGroupId  
-			   WHERE JT.JobNumber=j.JobNumber AND jg.Id=1 and JT.StatusDate >= DATEADD(M,-3,getdate()) -- will  always get lessthan 3 months data..
-			   GROUP BY jg.Id,jt.JobNumber,JG.StatusGroup) JTI
+		 JP.FirstName, JP.LastName,
+		 CASE WHEN JSB.id = 5 THEN 
+				CASE WHEN JSB.DeliveredOn is null THEN 
+					JTA.StatusGroup + ': '+ CONVERT(varchar(75), JTA.StatusDate, 100) 
+				ELSE 
+					JSB.DeliveredOn 
+				END
+		ELSE 
+			JSB.JobStatus 
+		END JobStatus,
+		 JH.StatusDate AS 'InProcess', 
+		 JTA.StatusDate AS 'AwaitingDelevery'
+		 FROM @JobsToFilter JH 
+		 INNER JOIN jobs j on j.jobnumber = jH.jobnumber  and j.clinicId=JH.ClinicID
+		 INNER JOIN dbo.Jobs_Patients JP ON JH.JobNumber = JP.JobNumber 
+		 INNER JOIN dbo.Dictators D ON J.DictatorID = D.DictatorID
 			OUTER APPLY(SELECT JT.JobNumber,MAX(jt.StatusDate) StatusDate,jg.Id,JG.StatusGroup
-			   FROM dbo.JobTracking JT  
-			   INNER JOIN dbo.StatusCodes SC on JT.Status= SC.StatusID
-			   INNER JOIN dbo.JobStatusGroup JG on JG.Id = SC.StatusGroupId  
-			   WHERE  JT.JobNumber=j.JobNumber AND jg.Id=4 and JT.StatusDate >= DATEADD(M,-3,getdate()) -- will  always get lessthan 3 months data..-- 4 is for status group for Awaiting Delivery
-			   GROUP BY jg.Id,jt.JobNumber,JG.StatusGroup) JTA
-			OUTER APPLY(SELECT case when JSA.StatusDate is null then (JG.StatusGroup + ': '+ CONVERT(varchar(75), JSB.StatusDate, 100)) 
-				  ELSE (JG.StatusGroup + ': '+ CONVERT(varchar(75), JSA.StatusDate, 100)) end JobStatus, JG.StatusGroup,JG.Id 
-			   FROM  dbo.JobStatusGroup JG 
-			   LEFT outer JOIN dbo.JobStatusA JSA on JSA.JobNumber = J.JobNumber 
-			   LEFT OUTER JOIN dbo.JobStatusB JSB on JSB.JobNumber = J.JobNumber
-			   INNER JOIN dbo.StatusCodes SC on JSA.Status= SC.StatusID OR JSB.Status= SC.StatusID
-			   WHERE JG.Id = SC.StatusGroupId ) JS
-			OUTER APPLY(SELECT JG.StatusGroup + ': '+ CONVERT(varchar(75), max(JD.DeliveredOn), 100) JobDeliveredOn, JG.StatusGroup,JG.Id 
-				FROM  dbo.JobStatusGroup JG 
-				LEFT OUTER JOIN dbo.JobStatusB JSB on JSB.JobNumber = J.JobNumber
-				INNER JOIN dbo.StatusCodes SC on JSB.Status= SC.StatusID
-				INNER JOIN dbo.JobDeliveryHistory JD on JD.jobnumber=j.jobnumber
-				WHERE JG.Id = SC.StatusGroupId 
-				group by JD.jobnumber,JG.StatusGroup,JG.Id ) JD
-		WHERE 
-			(@DateField is null or JTI.Id= @DateField) 
-			and (@From is null or JTI.StatusDate  >= @From)
-			and (@To is null or JTI.StatusDate  <= @To) 
-			and (@RangeOptionFromDate is null or JTI.StatusDate  >= @RangeOptionFromDate) 
-			and (@RangeOptionToDate is null or JTI.StatusDate  <= @RangeOptionToDate) 
-			and (@JobType is null or J.JobType = @JobType) 
-			and (@JobStatus is null or JS.Id = @JobStatus) 
-			and (@DictatorID is null or J.DictatorID = @DictatorID) 
-			and (@MRN is null or JP.MRN = @MRN) 
-			and (@FirstName is null or JP.FirstName = @FirstName) 
-			and (@LastName is null or JP.LastName = @LastName) 
-			and (@DeviceGenerated is null or J.IsGenericJob = @DeviceGenerated) 
-			and (@CC is null or J.CC = @CC) 
-			and (@STAT is null or J.Stat = @STAT) 
-			and (@ClinicID is null or J.ClinicID = @ClinicID)  
-			and (@JobNumber is null or J.JobNumber = @JobNumber)  
-			and (@DictatorFirstName is null or D.FirstName like '%'+@DictatorFirstName+'%')  
-			and (@DictatorLastName is null or D.LastName like '%'+@DictatorLastName+'%')  
-			and (J.ReceivedOn  >= DATEADD(M,-3,getdate())) --  >= DATEADD(M,-4,getdate()-- will  always get lessthan 3 months data..
-	
+		  FROM dbo.JobTracking JT  
+		  INNER JOIN dbo.StatusCodes SC on JT.Status= SC.StatusID
+		  INNER JOIN dbo.JobStatusGroup JG on JG.Id = SC.StatusGroupId  
+		  WHERE  JT.JobNumber=JH.JobNumber AND jg.Id=4 and JT.StatusDate >= DATEADD(M,-3,getdate()) -- will  always get lessthan 3 months data..-- 4 is for status group for Awaiting Delivery
+		  GROUP BY jg.Id,jt.JobNumber,JG.StatusGroup) JTA
+		   CROSS APPLY(SELECT case when JSA.StatusDate is null then (JG.StatusGroup + ': '+ CONVERT(varchar(75), JSB.StatusDate, 100)) 
+				  ELSE (JG.StatusGroup + ': '+ CONVERT(varchar(75), JSA.StatusDate, 100)) end JobStatus, JG.StatusGroup,JG.Id,JG.StatusGroup + ': '+ CONVERT(varchar(75), max(JD.DeliveredOn), 100) DeliveredOn 
+				  FROM  dbo.JobStatusGroup JG 
+				  LEFT OUTER JOIN dbo.JobStatusA JSA on JSA.JobNumber = JH.JobNumber 
+				  LEFT OUTER JOIN dbo.JobStatusB JSB on JSB.JobNumber =JH.JobNumber 
+				  INNER JOIN dbo.StatusCodes SC on (JSA.Status= SC.StatusID OR JSB.Status= SC.StatusID) and JG.Id = SC.StatusGroupId
+				  LEFT OUTER JOIN dbo.JobDeliveryHistory JD on JD.jobnumber=JH.JobNumber 
+				  WHERE (jg.id in (1,2,3,4) or (@jobStatus =4 or @jobStatus is null or @jobStatus =5 and JH.JobNumber  in (select jobnumber from JobDeliveryHistory where jobnumber=JH.JobNumber )))
+				  GROUP BY JG.StatusGroup,JG.Id,JSB.StatusDate,JSA.StatusDate,JD.DeliveredOn)  JSB
+		WHERE	((@JobStatus is null or JSB.Id = @JobStatus) OR (JSB.DeliveredOn is null and JTA.id=@JobStatus))			
+				and (@MRN is null or JP.MRN = @MRN) 
+				and (@FirstName is null or JP.FirstName = @FirstName) 
+				and (@LastName is null or JP.LastName = @LastName) 
+				and (@DictatorFirstName is null or D.FirstName like '%'+@DictatorFirstName+'%')  
+				and (@DictatorLastName is null or D.LastName like '%'+@DictatorLastName+'%')  
+
           Select Jobnumber, DictatorID, JobType, case when IsGenericJob is not null and IsGenericJob=1 then 'Y' else 'N' end DeviceGenerated,
 		  AppointmentDate, CC, Stat, MRN, Patient, JobStatus, InProcess, AwaitingDelivery
 		  from #SearchItems
