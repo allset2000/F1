1eca25a048d6aeb8ea0c338fbf9fe6e7f28d09d1
raghuarvapally1 <raghu.arvapally@valuelabs.co>
2015-10-20 13:19:10 +0530

    #3450 bug fixed

--- HostedClient/Stored Procedures/dbo.sp_GetMessageThreadByID.sql
+++ HostedClient/Stored Procedures/dbo.sp_GetMessageThreadByID.sql
@@ -1,17 +1,36 @@
-SET QUOTED_IDENTIFIER ON
-GO
+/****** Object:  StoredProcedure [dbo].[sp_GetMessageThreadByID]    Script Date: 10/16/2015 20:58:51 ******/
 SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
 GO
 
 -- =============================================
 -- Author: Samuel Shoultz
 -- Create date: 3/23/2015
 -- Description: SP used to pull the MessageThread by threadid
+--Modified By :Raghu, A Raghu
+--Modified Date : 07/28/2015,10/16/2016
+--Decription: ThreadAdminUserID added
+---Added patient Name 
 -- =============================================
-CREATE PROCEDURE [dbo].[sp_GetMessageThreadByID]
+--exec sp_GetMessageThreadByID '56250cbd31c5595a82000b9a'
+ALTER PROCEDURE [dbo].[sp_GetMessageThreadByID]
 	@ThreadId AS VARCHAR(100)
 AS 
 BEGIN
-	SELECT MessageThreadId, ThreadID, ThreadOwnerID, PatientID, IsActive, PassPhrase FROM MessageThreads WHERE ThreadID = @ThreadId
+	   SELECT 
+		   MH.MessageThreadId, 
+		   MH.ThreadID, 
+		   MH.ThreadOwnerID,
+		   MH.PatientID,
+		   MH.IsActive,
+		   MH.PassPhrase,
+		   MH.ThreadAdminUserID,		  
+		   MH.CreatedDate,
+		   P.FirstName +' '+P.MI+' '+P.LastName AS PatientName
+	   FROM MessageThreads MH WITH(NOLOCK)	
+	   LEFT JOIN Patients p WITH(NOLOCK) ON P.PatientID=MH.PatientID
+	   WHERE MH.ThreadID = @ThreadId
 END
-GO
+
+
