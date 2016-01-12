34aca312657e201df6bc4af2c7d5682c8b0c194b
Santhosh <smukkamala@entradahealth.com>
2016-01-12 18:22:05 +0530

    #365# - Adding JobsToDeliver Codes

--- Backend/Tables/dbo.JobsToDeliverErrors.sql
+++ Backend/Tables/dbo.JobsToDeliverErrors.sql
@@ -8,16 +8,15 @@ CREATE TABLE [dbo].[JobsToDeliverErrors]
 [Message] [varchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [ErrorMessage] [varchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [ExceptionMessage] [varchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[StackTrace] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[StackTrace] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ErrorCode] [int] NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-CREATE NONCLUSTERED INDEX [IX_JobsToDeliverErrors_DeliveryID] ON [dbo].[JobsToDeliverErrors] ([DeliveryId], [ErrorDeliveryId]) ON [PRIMARY]
-
-CREATE NONCLUSTERED INDEX [IX_JobsToDeliverErrors_ErrorDate] ON [dbo].[JobsToDeliverErrors] ([ErrorDate]) ON [PRIMARY]
-
-
-
-CREATE NONCLUSTERED INDEX [IX_ErrorMessage_INC] ON [dbo].[JobsToDeliverErrors] ([ErrorMessage]) INCLUDE ([DeliveryId]) ON [PRIMARY]
-
 GO
 ALTER TABLE [dbo].[JobsToDeliverErrors] ADD CONSTRAINT [PK_JobsToDeliverErrors] PRIMARY KEY CLUSTERED  ([ErrorDeliveryId]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_JobsToDeliverErrors_DeliveryID] ON [dbo].[JobsToDeliverErrors] ([DeliveryId], [ErrorDeliveryId]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_JobsToDeliverErrors_ErrorDate] ON [dbo].[JobsToDeliverErrors] ([ErrorDate]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_ErrorMessage_INC] ON [dbo].[JobsToDeliverErrors] ([ErrorMessage]) INCLUDE ([DeliveryId]) ON [PRIMARY]
 GO
