429844a8692aa541e906e28b03b151357cbc4843
DDorsey <ddorsey@entradahealth.com>
2016-01-22 13:56:48 -0600

    #000# Adding missing FK and indexes on Job_History

--- Backend/Tables/dbo.Job_History.sql
+++ Backend/Tables/dbo.Job_History.sql
@@ -14,6 +14,14 @@ CREATE TABLE [dbo].[Job_History]
 [DOB] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [IsHistory] [bit] NOT NULL CONSTRAINT [DF_Job_History_IsHistory] DEFAULT ((0))
 ) ON [PRIMARY]
+CREATE NONCLUSTERED INDEX [IX_Job_History_DocumentId] ON [dbo].[Job_History] ([DocumentID]) ON [PRIMARY]
+
+CREATE NONCLUSTERED INDEX [IX_Job_History_JobNumber] ON [dbo].[Job_History] ([JobNumber]) ON [PRIMARY]
+
+ALTER TABLE [dbo].[Job_History] ADD
+CONSTRAINT [FK_Job_History_DocumentID] FOREIGN KEY ([DocumentID]) REFERENCES [dbo].[Jobs_Documents_History] ([DocumentID])
+ALTER TABLE [dbo].[Job_History] ADD
+CONSTRAINT [FK_Job_History_JobNumber] FOREIGN KEY ([JobNumber]) REFERENCES [dbo].[Jobs] ([JobNumber])
 ALTER TABLE [dbo].[Job_History] ADD 
 CONSTRAINT [PK_Job_History] PRIMARY KEY CLUSTERED  ([JobHistoryID]) ON [PRIMARY]
 
