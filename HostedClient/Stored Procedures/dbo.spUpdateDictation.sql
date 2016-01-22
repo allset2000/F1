2059c7fc1108d34c0484ea65c062fe0f973ac31e
Santhosh <smukkamala@entradahealth.com>
2016-01-22 18:18:07 +0530

    #358# - (RM-4672) PFC - Support Linked Appointments for 4 NextGen Resources

--- HostedClient/Stored Procedures/dbo.spUpdateDictation.sql
+++ HostedClient/Stored Procedures/dbo.spUpdateDictation.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -44,29 +45,6 @@ BEGIN TRY
 		   QueueID=@vintQueueID,Duration=@smintDuration,MachineName=@vvcrMachineName,UpdatedDateInUTC=GETUTCDATE()
 	WHERE DictationID = @bgintDictationID  
 
-	-- Only for NextGen Vendor
-	DECLARE @EncounterID BIGINT
-	DECLARE @JobNumber VARCHAR(20)
-	DECLARE @RuleId SMALLINT
-
-	SELECT @EncounterID = J.EncounterID, @JobNumber = J.JobNumber, @RuleId = J.RuleID 
-	FROM Dictations D
-		INNER JOIN Jobs J ON J.JobID = D.JobID
-		INNER JOIN Clinics C ON C.ClinicID = J.ClinicID AND C.EHRVendorID = 3
-		INNER JOIN Rules R ON R.RuleID = J.RuleID AND R.RemoveAppointments = 1
-	WHERE D.DictationID = @bgintDictationID
-	
-	UPDATE Jobs
-	SET Status = 500
-	FROM Jobs J INNER JOIN Dictations D ON D.JobID = J.JobID AND D.Status = 100
-	WHERE J.EncounterID = @EncounterID AND J.RuleID = @RuleId AND J.JobNumber <> @JobNumber
-
-	UPDATE Dictations
-	SET Status = 500
-	FROM Dictations D INNER JOIN Jobs J ON D.JobID = J.JobID AND D.Status = 100
-	WHERE J.EncounterID = @EncounterID AND J.RuleID = @RuleId AND J.JobNumber <> @JobNumber
-	-- Only for NextGen Vendor
-	
 	-- Only update if the status changed
 	IF (@OldDictationStatus <> @vintStatus)
 		INSERT INTO DictationsTracking(DictationID,Status,ChangeDate,ChangedBy)
@@ -80,6 +58,43 @@ BEGIN TRY
 		INSERT INTO Jobstracking(JobID,Status,ChangeDate,ChangedBy)
 		VALUES(@bgintjobId,@newJobStatus,getdate(),@vvcrChangedBy)	
 			
+
+	--This is to delete jobs which have same encounterid and ruleid as this dictated job for NextGen vendor only
+	IF(@vintStatus = 250)
+	BEGIN
+		-- Only for NextGen Vendor
+		DECLARE @EHREncounterID VARCHAR(50)
+		DECLARE @JobNumber VARCHAR(20)
+		DECLARE @RuleId SMALLINT
+
+		SELECT @EHREncounterID = S.EHREncounterID, @JobNumber = J.JobNumber, @RuleId = J.RuleID 
+		FROM Dictations D
+			INNER JOIN Jobs J ON J.JobID = D.JobID
+			INNER JOIN Encounters E ON E.EncounterID = J.EncounterID
+			INNER JOIN Schedules S ON S.ScheduleID = E.ScheduleID
+			INNER JOIN Clinics C ON C.ClinicID = J.ClinicID AND C.EHRVendorID = 3
+			INNER JOIN Rules R ON R.RuleID = J.RuleID AND R.RemoveAppointments = 1
+		WHERE D.DictationID = @bgintDictationID		
+	
+		UPDATE Jobs
+		SET Status = 500, UpdatedDateInUTC = GETUTCDATE()
+		FROM Jobs J 
+			INNER JOIN Dictations D ON D.JobID = J.JobID AND D.Status = 100
+			INNER JOIN Clinics C ON C.ClinicID = J.ClinicID AND C.EHRVendorID = 3
+			INNER JOIN Encounters E ON E.EncounterID = J.EncounterID
+			INNER JOIN Schedules S ON S.ScheduleID = E.ScheduleID AND S.EHREncounterID = @EHREncounterID
+		WHERE J.RuleID = @RuleId AND J.JobNumber <> @JobNumber
+
+		UPDATE Dictations
+		SET Status = 500, UpdatedDateInUTC = GETUTCDATE()
+		FROM Dictations D 
+			INNER JOIN Jobs J ON D.JobID = J.JobID AND D.Status = 100
+			INNER JOIN Clinics C ON C.ClinicID = J.ClinicID AND C.EHRVendorID = 3
+			INNER JOIN Encounters E ON E.EncounterID = J.EncounterID
+			INNER JOIN Schedules S ON S.ScheduleID = E.ScheduleID AND S.EHREncounterID = @EHREncounterID
+		WHERE J.RuleID = @RuleId AND J.JobNumber <> @JobNumber
+		-- Only for NextGen Vendor
+	END
 	COMMIT TRANSACTION
 END TRY
 BEGIN CATCH
