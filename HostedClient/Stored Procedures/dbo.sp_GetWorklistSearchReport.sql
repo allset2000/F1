8227c71ac7e1980304718f4c834714186757bd89
rnarender <ramadheni.narender@adroitent.com>
2016-01-29 19:14:52 +0530

    #392# Updated the SP to return WorkList report for selected filters

--- HostedClient/Stored Procedures/dbo.sp_GetWorklistSearchReport.sql
+++ HostedClient/Stored Procedures/dbo.sp_GetWorklistSearchReport.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -27,7 +28,89 @@ BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	SET NOCOUNT ON;
 
+	Declare @JobTypeID int
+
+    IF OBJECT_ID('tempdb..#WorkListSearchItems') IS NOT NULL
+		DROP TABLE #WorkListSearchItems
 	
+	select @JobTypeID = JobTypeID from JobTypes where Name = @JobType and clinicID = @clinicID
+
+	CREATE TABLE #WorkListSearchItems (
+			--JobNumber varchar(20),
+			QueueName varchar(50) NULL,
+			JobType varchar(100) NULL,
+			MRN varchar(50) NULL,
+			FirstName varchar(100),
+			LastName varchar(100),
+			Patient varchar(100),
+			Appointment DateTime null,
+			--changeDate DateTime,
+			ChangedBy varchar(50) null,
+			Status smallint null)
+
+
+	INSERT INTO #WorkListSearchItems
+		SELECT 
+		--J.JobNumber,
+		Q.Name AS QueueName,
+		JobTypes.Name AS JobType,
+		P.MRN AS MRN, 
+		P.FirstName AS FirstName, 
+		P.LastName AS LastName,
+		(P.MRN + ' - ' + P.FirstName + ' ' + P.LastName) AS Patient,
+		E.AppointmentDate AS Appointment,
+		--JT.ChangeDate,
+		JT.ChangedBy AS ChangedBy,
+		J.Status AS Status
+			FROM Jobs J 
+			INNER JOIN JobTypes ON J.JobTypeID = JobTypes.JobTypeID 
+			INNER JOIN Encounters E ON J.EncounterID = E.EncounterID 
+			INNER JOIN Patients P ON E.PatientID = P.PatientID 
+			INNER JOIN Dictations D ON D.JobID = J.JobId 
+			LEFT JOIN Queues Q ON D.QueueID = Q.QueueID 
+			LEFT JOIN Dictators DI ON D.DictatorID = DI.DictatorID
+			INNER JOIN JobsTracking JT ON JT.jobid = J.jobID 
+			WHERE 
+			(@ClinicID is null or J.ClinicID = @ClinicID)
+			AND (CAST(@QueueID as int) is null or D.QueueID = CAST(@QueueID as int))
+			AND (CAST(@JobType as BIGINT)  is null or J.JobTypeID = CAST(@JobType as BIGINT))
+			AND (CAST(@Status as SMALLINT) is null or (JT.Status = CAST(@Status as SMALLINT) AND (J.Status IN(100,500))))
+			AND (CONVERT(DateTime, @from) is null or E.AppointmentDate >= CONVERT(DateTime, @from)) -- need to confirm whether to get less than 3 months data
+			AND (CONVERT(DateTime, @to) is null or E.AppointmentDate <= CONVERT(DateTime, @to))
+			AND (@MRN is null or P.MRN = @MRN)
+			AND (@FirstName is null or P.FirstName LIKE '%' + @FirstName + '%')
+			AND (@LastName is null or P.LastName LIKE '%' + @LastName + '%')
+
+			
+	SELECT QueueName,JobType,MRN,FirstName,LastName, Patient,Appointment,ChangedBy, 
+		CASE WHEN Status is not null and Status = 100 THEN 'Available' ELSE 'Deleted' END Status FROM #WorkListSearchItems
+	ORDER BY 
+	CASE WHEN @SortTypeFromGrid = 'Ascending' THEN 
+					CASE @SortColumnFromGrid
+					WHEN 'Patient' THEN Patient
+					WHEN 'JobType' THEN JobType
+					WHEN 'Queue' THEN QueueName
+					WHEN 'Appointment' THEN (CONVERT(varchar, Appointment)) 
+					WHEN 'Status' THEN (CONVERT(varchar,Status))
+					WHEN 'ChangedBy' THEN ChangedBy
+					END
+				END,
+				CASE WHEN @SortTypeFromGrid = 'Descending' THEN
+					CASE @SortColumnFromGrid 
+					WHEN 'Patient' THEN Patient
+					WHEN 'JobType' THEN JobType
+					WHEN 'Queue' THEN QueueName
+					WHEN 'Appointment' THEN (CONVERT(varchar, Appointment)) 
+					WHEN 'Status' THEN (CONVERT(varchar,Status))
+					WHEN 'ChangedBy' THEN ChangedBy
+					END
+				END DESC
+		OFFSET (@PageNo - 1) * @PageSize ROWS
+		FETCH NEXT @PageSize ROWS ONLY
+	
+	SELECT @TotalCount = Count(QueueName) from #WorkListSearchItems
+
+	DROP TABLE #WorkListSearchItems
     
 END
 GO
