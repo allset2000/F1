dbc5239babc89a162864137a7668e7da0c5c72f2
Santhosh <smukkamala@entradahealth.com>
2016-02-03 19:17:46 +0530

    #365# - (RM-2241) Add JobsToDeliver Error Codes

--- Backend/Stored Procedures/dbo.sp_LogJobDeliveryFailure.sql
+++ Backend/Stored Procedures/dbo.sp_LogJobDeliveryFailure.sql
@@ -9,18 +9,18 @@ GO
 -- Description: SP used to log Job Delivery Failure errors
 -- =============================================    
 CREATE PROCEDURE [dbo].[sp_LogJobDeliveryFailure](
-      @ConfigurationName  VARCHAR(100) = NULL,
       @DeliveryId INT = NULL,
-      @ErrorId SMALLINT = NULL,
 	  @Message VARCHAR(200) = NULL,
-	  @ErrorMessage VARCHAR(200),
+	  @ConfigurationName  VARCHAR(100) = NULL,      
+      @ErrorId SMALLINT = NULL,	  
+	  @ErrorMessage VARCHAR(200) = NULL,
       @ExceptionMessage VARCHAR(200) = NULL,
       @StackTrace VARCHAR(MAX) = NULL
       
 )AS 
 BEGIN	
 	DECLARE @ErrorCode INT
-	SET @ErrorCode = ISNULL((SELECT TOP 1 ED.ErrorCode from ENTRADAHOSTEDCLIENT.DBO.ErrorDefinitions ED where @ErrorMessage like '%'+ED.ErrorMessage+'%'),5000)
+	SET @ErrorCode = ISNULL((SELECT TOP 1 ED.ErrorCode from ENTRADAHOSTEDCLIENT.DBO.ErrorDefinitions ED where @Message like '%'+ED.ErrorMessage+'%'),5000)
 	
 	INSERT INTO [dbo].[JobsToDeliverErrors]
 			   ([ConfigurationName]
