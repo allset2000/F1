58278c49235e83a43f23824e62759eaec00510da
Raghu Arvapally <raghu.arvapally@valuelabs.com>
2016-02-05 22:19:17 +0530

    #000# modular sync recursive logic implemented

--- HostedClient/Stored Procedures/dbo.sp_GetEncountersToSyncByLastSyncDate.sql
+++ HostedClient/Stored Procedures/dbo.sp_GetEncountersToSyncByLastSyncDate.sql
@@ -8,7 +8,7 @@ GO
 -- Author: Raghu A
 -- Create date: 18/11/2014
 -- Description: SP called from DictateAPI to pull Dictations to sync on mobile
---exec sp_GetEncountersToSyncByLastSyncDate 2196 ,'2014-11-20 10:15:02.970','2015-05-04 00:00:00.000','prev'
+--exec sp_GetEncountersToSyncByLastSyncDate 2196 ,'2014-11-20 10:15:02.970','2015-05-04 00:00:00.000','next'
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_GetEncountersToSyncByLastSyncDate](
 	 @DictatorId INT,
@@ -18,15 +18,20 @@ CREATE PROCEDURE [dbo].[sp_GetEncountersToSyncByLastSyncDate](
 ) AS 
 BEGIN
      SET NOCOUNT ON;
-  
-  DECLARE @repeatvalue INT=1
 
-  StartPoint: --Don't delete this string we are using this for repetition task Up to 7
-    
-	IF OBJECT_ID('tempdb..#TempEncounters') IS NOT NULL
-		 DROP TABLE #TempEncounters
-
-	SELECT DISTINCT e.EncounterID AS ID, 
+	    DECLARE @TempEncounter AS TABLE	
+				(ID INT,
+				 AppointmentDate INT,
+				 PatientID INT,
+				 ScheduleID INT,
+				 [State] INT,
+				 JobDetails VARCHAR(2000)
+				)
+	 
+   StartPoint: --Don't delete this string we are using this for repetition task Up to 7
+ 
+	  INSERT INTO @TempEncounter
+	  SELECT DISTINCT e.EncounterID AS ID, 
 			 DATEDIFF(SECOND,{D '1970-01-01'}, e.AppointmentDate) AS AppointmentDate ,			
 			 e.PatientID, 
 			 e.ScheduleID,
@@ -35,7 +40,7 @@ BEGIN
 				  FROM   dbo.Jobs j2 
 				  WHERE  j2.EncounterID = e.EncounterID                   
 				  FOR XML PATH('')), 1, 2, '')  JobDetails
-		INTO #TempEncounters FROM dbo.Encounters e
+		 FROM dbo.Encounters e
 				INNER	JOIN dbo.Jobs j ON j.EncounterID=e.EncounterID
 				INNER JOIN dbo.Dictations d ON d.JobID=j.JobID	
 				INNER JOIN dbo.Queue_Users AS qu ON qu.QueueID = d.QueueID 
@@ -54,25 +59,35 @@ BEGIN
 
     
 	    --this loic for repeat the task upto 7 times,and it will only if direction value come as a next or prev
-	      IF (NOT EXISTS(SELECT	'*' FROM #TempEncounters) AND (ISNULL(@Direction,'')<>'' AND @repeatvalue<=7))
-		    BEGIN
+	      IF (NOT EXISTS(SELECT	'*' FROM @TempEncounter) AND (ISNULL(@Direction,'')<>'' ))
+		    BEGIN			
 
-			     IF @Direction='prev'
-				   SET @AppointmentDate=DATEADD(DD,-1,@AppointmentDate)
-				 ELSE
-				   SET @AppointmentDate=DATEADD(DD,1,@AppointmentDate)
+			   SELECT @AppointmentDate=(CASE WHEN @Direction='prev' THEN MAX(e.AppointmentDate) ELSE MIN (e.AppointmentDate) END) 
+				 FROM dbo.Encounters e
+						INNER	JOIN dbo.Jobs j ON j.EncounterID=e.EncounterID
+						INNER JOIN dbo.Dictations d ON d.JobID=j.JobID	
+						INNER JOIN dbo.Queue_Users AS qu ON qu.QueueID = d.QueueID 
+						INNER JOIN dbo.Queues AS q ON q.QueueID = qu.QueueID
+						LEFT JOIN dbo.Schedules s ON s.ScheduleID=e.ScheduleID
+						LEFT JOIN dbo.Patients p ON p.PatientID=e.PatientID
+				WHERE qu.DictatorID = @DictatorId AND 
+		     	      @AppointmentDate<=(CASE WHEN @Direction='next' THEN e.AppointmentDate else @AppointmentDate END) AND
+					  @AppointmentDate>=(CASE WHEN @Direction='prev' THEN e.AppointmentDate else @AppointmentDate END) AND
+					  d.[Status] IN (100, 200) AND 			  
+					  (ISNULL(e.UpdatedDateInUTC,GETUTCDATE())>@LastSyncDate
+					   OR (e.ScheduleID  IS NOT NULL AND ISNULL(s.UpdatedDateInUTC,GETUTCDATE())>@LastSyncDate)
+					   OR (e.PatientID IS NOT NULL AND ISNULL(p.UpdatedDateInUTC,GETUTCDATE())>@LastSyncDate)
+					   OR (ISNULL(j.UpdatedDateInUTC,GETUTCDATE())>@LastSyncDate)
+			           )
 
-				   SET @repeatvalue=@repeatvalue+1
-
-				 -- SELECT @repeatvalue,@AppointmentDate
-
-				 --this step goto the start position and repeat the task
-				   GOTO StartPoint;
+            --if there is appointment date for previous or next it this value comes has a null
+			 IF(@AppointmentDate IS NOT NULL)			  
+				  GOTO StartPoint; --this step goto the start position and repeat the task
 
 			END
 	 
 
-	SELECT ID, AppointmentDate, PatientID, ScheduleID, [State], JobDetails  FROM #TempEncounters   
+	SELECT ID, AppointmentDate, PatientID, ScheduleID, [State], JobDetails  FROM @TempEncounter   
 
 
 END
