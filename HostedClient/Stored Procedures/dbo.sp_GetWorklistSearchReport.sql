07a82d487ddafae941748996c594cb49d1185fa3
rnarender <ramadheni.narender@adroitent.com>
2016-02-12 20:27:08 +0530

    #392# Updated the SP sp_GetWorklistSearchReport with review comments

--- HostedClient/Stored Procedures/dbo.sp_GetWorklistSearchReport.sql
+++ HostedClient/Stored Procedures/dbo.sp_GetWorklistSearchReport.sql
@@ -7,6 +7,7 @@ GO
 -- Author:		Narender
 -- Create date: 01/27/2016
 -- Description:	This SP will return WorkList for selected search filter
+--EXEC sp_GetWorklistSearchReport @clinicID=1,@PageNo=1,@SortColumnFromGrid='Patient',@SortTypeFromGrid='Descending',@PageSize=30,@TotalCount out
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_GetWorklistSearchReport] 
 @clinicID smallint,
@@ -14,56 +15,48 @@ CREATE PROCEDURE [dbo].[sp_GetWorklistSearchReport]
 @SortColumnFromGrid varchar(50)= null,
 @SortTypeFromGrid   varchar(50)= null,
 @PageSize smallint,
-@QueueID varchar(50) = null,
-@JobType varchar(100) = null,
-@Status varchar(50) = null,
+@QueueID INT=null,
+@JobType BIGINT = null,
+@Status SMALLINT = null,
 @From varchar(50) = null,
 @To varchar(50)= null,
 @MRN varchar(50) = null,
 @FirstName varchar(100) = null,
-@LastName varchar(100) = null,
-@TotalCount int Output
+@LastName varchar(100) = null
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	SET NOCOUNT ON;
 
-	DECLARE @JobTypeID int
-	DECLARE @RangeOptionFromDate datetime
-	DECLARE @RangeOptionToDate datetime
+		 DECLARE @Sql NVarchar(max)=''	
+   BEGIN TRY
 
-    IF OBJECT_ID('tempdb..#WorkListSearchItems') IS NOT NULL
-		DROP TABLE #WorkListSearchItems
+	 IF (@SortTypeFromGrid='Descending')
+		   BEGIN
+			  SET @SortTypeFromGrid='DESC'	   
+		   END
+		ELSE
+		   BEGIN
+			  SET @SortTypeFromGrid='ASC'
+		   END   
+		SELECT @SortColumnFromGrid = CASE WHEN @SortColumnFromGrid='DictatorId' THEN 'DictatorId'
+								WHEN  @SortColumnFromGrid='Patient' THEN 'Patient'
+								WHEN @SortColumnFromGrid='JobType' THEN 'JobType'
+								WHEN @SortColumnFromGrid='Queue' THEN 'Queue'
+								WHEN @SortColumnFromGrid='Appointment' THEN 'AppointmentDate'
+								WHEN @SortColumnFromGrid='Status' THEN 'Status'
+								WHEN @SortColumnFromGrid='ChangedBy' THEN 'ChangedBy'
+								ELSE 'Patient' END
+
+	 SET @Sql=';WITH Worklist_CTE AS ( 
+				     				SELECT 
 	
-	select @JobTypeID = JobTypeID from JobTypes where Name = @JobType and clinicID = @clinicID
-
-	CREATE TABLE #WorkListSearchItems (
-			QueueName varchar(50) NULL,
-			JobType varchar(100) NULL,
-			Patient varchar(100),
-			Appointment DateTime null,
-			ChangedBy varchar(50) null,
-			Status smallint null)
-
-	IF(@From is not null OR @To is not null)
-		BEGIN
-			SELECT @RangeOptionFromDate = CONVERT(varchar, @From, 101)+' 00:00:00'
-			SELECT @RangeOptionToDate = CONVERT(varchar, @To, 101) +' 23:59:59'
-		END
-	ELSE
-		BEGIN
-			SELECT @RangeOptionFromDate =  CONVERT(varchar, @From, 120)
-			SELECT @RangeOptionToDate =  CONVERT(varchar, @To, 120)
-		END	
-
-	INSERT INTO #WorkListSearchItems
-		SELECT 
-		Q.Name AS QueueName,
+		Q.Name AS Queue,
 		JobTypes.Name AS JobType,
-		(P.MRN + ' - ' + P.FirstName + ' ' + P.LastName) AS Patient,
+		(P.MRN + '' - '' + P.FirstName + '' '' + P.LastName) AS Patient,
 		E.AppointmentDate AS Appointment,
 		JT.ChangedBy AS ChangedBy,
-		J.Status AS Status
+		J.Status AS Status, COUNT(*) OVER() as TotalCount
 			FROM Jobs J 
 			INNER JOIN JobTypes ON J.JobTypeID = JobTypes.JobTypeID 
 			INNER JOIN Encounters E ON J.EncounterID = E.EncounterID 
@@ -73,49 +66,56 @@ BEGIN
 			LEFT JOIN Dictators DI ON D.DictatorID = DI.DictatorID
 			LEFT OUTER JOIN (Select JobID,Status,MAX(ChangeDate) AS MAX_ChangeDate,ChangedBy FROM JobsTracking GROUP BY JobID,Status,ChangedBy) JT 
 						ON J.jobid = JT.jobID AND J.status = JT.status
-			WHERE 
-			(@ClinicID is null or J.ClinicID = @ClinicID)
-			AND (CAST(@QueueID as int) is null or D.QueueID = CAST(@QueueID as int))
-			AND (CAST(@JobType as BIGINT)  is null or J.JobTypeID = CAST(@JobType as BIGINT))
-			AND (CAST(@Status as SMALLINT) is null or J.Status = CAST(@Status as SMALLINT)) 
-			AND (J.Status IN(100,500))
-			AND (CONVERT(DateTime, @RangeOptionFromDate) is null or E.AppointmentDate >= CONVERT(DateTime, @RangeOptionFromDate)) -- need to confirm whether to get less than 3 months data
-			AND (CONVERT(DateTime, @RangeOptionToDate) is null or E.AppointmentDate <= CONVERT(DateTime, @RangeOptionToDate))
-			AND (@MRN is null or P.MRN = @MRN)
-			AND (@FirstName is null or P.FirstName LIKE '%' + @FirstName + '%')
-			AND (@LastName is null or P.LastName LIKE '%' + @LastName + '%')
-
+			WHERE J.Status IN(100,500) AND J.ClinicID = ' +CAST(@clinicID AS VARCHAR)
 			
-	SELECT QueueName,JobType,
-	 Patient,Appointment,ChangedBy, 
-		CASE WHEN Status is not null and Status = 100 THEN 'Available' ELSE 'Deleted' END Status FROM #WorkListSearchItems
-	ORDER BY 
-	CASE WHEN @SortTypeFromGrid = 'Ascending' THEN 
-					CASE @SortColumnFromGrid
-					WHEN 'Patient' THEN Patient
-					WHEN 'JobType' THEN JobType
-					WHEN 'Queue' THEN QueueName
-					WHEN 'Appointment' THEN (CONVERT(varchar, Appointment)) 
-					WHEN 'Status' THEN (CONVERT(varchar,Status))
-					WHEN 'ChangedBy' THEN ChangedBy
-					END
-				END,
-				CASE WHEN @SortTypeFromGrid = 'Descending' THEN
-					CASE @SortColumnFromGrid 
-					WHEN 'Patient' THEN Patient
-					WHEN 'JobType' THEN JobType
-					WHEN 'Queue' THEN QueueName
-					WHEN 'Appointment' THEN (CONVERT(varchar, Appointment)) 
-					WHEN 'Status' THEN (CONVERT(varchar,Status))
-					WHEN 'ChangedBy' THEN ChangedBy
-					END
-				END DESC
-		OFFSET (@PageNo - 1) * @PageSize ROWS
-		FETCH NEXT @PageSize ROWS ONLY
-	
-	SELECT @TotalCount = Count(QueueName) from #WorkListSearchItems
+			 
+				IF(@QueueID IS NOT NULL)
+					SET @sql += ' AND D.QueueID = ' + CAST(@QueueID AS VARCHAR)
+				IF(@JobType IS NOT NULL)
+					SET @sql += ' AND J.JobTypeID = ' + CAST(@JobType AS VARCHAR)
+				IF(@Status IS NOT NULL)
+					SET @sql += ' AND J.Status = ' + +CAST(@Status AS VARCHAR)
+				IF(@from IS NOT NULL)
+					SET @sql += ' AND E.AppointmentDate >= ' + CAST (@from as varchar)
+				IF(@To IS NOT NULL)
+					SET @sql += ' AND E.AppointmentDate <= ' + CAST (@To as varchar)
+				IF(@MRN IS NOT NULL)
+					SET @sql += ' AND P.MRN = ' + @MRN
+				IF(@FirstName IS NOT NULL)
+					SET @sql += ' AND P.FirstName like ''%' + @FirstName +'%'''
+				IF(@LastName IS NOT NULL)
+					SET @sql += ' AND P.LastName like ''%' + @LastName +'%'''
 
-	DROP TABLE #WorkListSearchItems
-    
+				SET @sql += ')'
+
+					SET @Sql=@Sql+' SELECT  Queue, JobType, Patient, Appointment, ChangedBy, 
+						CASE WHEN Status is not null and Status = 100 THEN ''Available'' when status = 500 then ''Deleted'' ELSE ''Unknown'' END Status,
+						TotalCount						 
+						FROM 
+							(SELECT ROW_NUMBER() OVER(ORDER BY '+@SortColumnFromGrid +' '+@SortTypeFromGrid+') as RowNumber,  
+								CTE.* 
+								FROM Worklist_CTE AS CTE
+								)A
+							ORDER BY RowNumber 
+							OFFSET (('+CAST(@PageNo AS VARCHAR)+'  - 1) * '+CAST(@PageSize AS VARCHAR) +' ) ROWS
+							FETCH NEXT '+CAST(@PageSize AS VARCHAR)+' ROWS ONLY  ' 
+						
+			   EXEC (@Sql)
+
+	END TRY
+	BEGIN CATCH
+	    
+		    DECLARE @errorMessage AS VARCHAR(4000) = (SELECT ERROR_MESSAGE()),
+					@errorSeverity AS INT = (SELECT ERROR_SEVERITY()),
+					@errorState AS INT = (SELECT ERROR_STATE())
+
+
+		INSERT INTO EH_LogExceptions
+				   (LogConfigurationID,ErrorThrownAtMethodName,ExceptionMessage,StackTrace,ErrorCreatedDate)
+		   VALUES  
+					(18,'sp_GetWorklistSearchReport',@errorMessage,@Sql,GETDATE())
+	
+	END CATCH
+
 END
 GO
