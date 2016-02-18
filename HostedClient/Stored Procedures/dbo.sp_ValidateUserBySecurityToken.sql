a05d505fd64542e1ee131f2b76c9f5043dfea2b4
Raghu Arvapally <raghu.arvapally@valuelabs.com>
2016-02-18 16:44:30 +0530

    #000# user invitation validation

---
+++ HostedClient/Stored Procedures/dbo.sp_ValidateUserBySecurityToken.sql
@@ -0,0 +1,16 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[sp_ValidateUserBySecurityToken]
+@SecurityToken VARCHAR(100)
+AS
+BEGIN
+      SET NOCOUNT ON;
+	
+	 SELECT COUNT(*) FROM dbo.UserInvitations U
+	                      INNER JOIN dbo.QuickBloxUsers qb ON qb.UserID=u.RequestingUserId
+						  WHERE u.SecurityToken=@SecurityToken
+	
+END
+GO
