466e24a2ad42c3e6df1a6d64b4e4d821e595a652
Sharif Shaik <sshaik@entradahealth.com>
2016-02-23 22:29:15 -0600

    #5477# - Image only ROW

--- HostedClient/Stored Procedures/dbo.sp_JobImagesGet.sql
+++ HostedClient/Stored Procedures/dbo.sp_JobImagesGet.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -10,7 +11,9 @@ GO
 --xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 /*
 	set statistics io on
-	exec sp_JobImagesGet 838920
+	exec sp_JobImagesGet 827790
+	-- Greenway: 827790
+	-- Athena: 838920
 */
 
 CREATE PROCEDURE [dbo].[sp_JobImagesGet] 
@@ -21,9 +24,16 @@ BEGIN
 
 	SET NOCOUNT ON;
 
-	SELECT JobImages.ImageID, JobImages.JobID, JobImages.ImagePath, JobImages.[Description]  
-	FROM JobImages LEFT JOIN JobsDeliveryTracking on JobImages.ImageID = JobsDeliveryTracking.ImageID 
-	WHERE JobImages.JobID = @JobID AND JobsDeliveryTracking.JobID IS NULL
+	--SELECT JobImages.ImageID, JobImages.JobID, JobImages.ImagePath, JobImages.[Description]  
+	--FROM JobImages LEFT JOIN JobsDeliveryTracking on JobImages.ImageID = JobsDeliveryTracking.ImageID 
+	--WHERE JobImages.JobID = @JobID AND JobsDeliveryTracking.JobID IS NULL
+
+	SELECT JobImages.ImageID, JobImages.JobID, JobImages.ImagePath, JobImages.ImageDescription  
+	FROM JobImages 
+	LEFT JOIN JobsDeliveryTracking on JobImages.ImageID = JobsDeliveryTracking.ImageID 
+	LEFT JOIN JobsDeliveryErrors on JobImages.ImageID = JobsDeliveryErrors.ImageID AND DATEDIFF(minute,ChangedOn,GETDATE()) < 30
+	WHERE JobImages.JobID = @JobID AND JobsDeliveryTracking.JobID IS NULL 
+	AND JobsDeliveryErrors.JobID IS NULL 
 
 END
 GO
--- HostedClient/Stored Procedures/dbo.sp_LogJobDeliveryFailure.sql
+++ HostedClient/Stored Procedures/dbo.sp_LogJobDeliveryFailure.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -20,18 +21,19 @@ BEGIN
 	  DECLARE @ErrorCode INT
 	  set @ErrorCode = ISNULL((SELECT TOP 1 ErrorCode from ErrorDefinitions where @ErrorMessage like '%'+ErrorDefinitions.ErrorMessage+'%'),5000)	  
 
-      IF EXISTS(select* from JobsDeliveryErrors where JobId = @JobId)
+      IF EXISTS(select* from JobsDeliveryErrors where JobId = @JobId and (@ImageID IS NULL or ImageID =  @ImageID))
       BEGIN
-            IF EXISTS(select* from JobsDeliveryErrors where JobId = @JobId and ErrorStatus = @ErrorStatus and ErrorMessage = @ErrorMessage)
+            IF EXISTS(select* from JobsDeliveryErrors where JobId = @JobId and ErrorStatus = @ErrorStatus and ErrorMessage = @ErrorMessage 
+						and (@ImageID IS NULL or ImageID =  @ImageID))
             BEGIN
                   UPDATE JobsDeliveryErrors set ChangedOn = GETDATE() where JobId = @JobId
             END
             ELSE
             BEGIN				  
-                  UPDATE JobsDeliveryErrors SET ErrorMessage = @ErrorMessage, ErrorStatus = @ErrorStatus, ExceptionMessage = @ExceptionMessage, ChangedOn = GETDATE(), ErrorCode = @ErrorCode where JobId = @JobId
+                  UPDATE JobsDeliveryErrors SET ErrorMessage = @ErrorMessage, ErrorStatus = @ErrorStatus, ExceptionMessage = @ExceptionMessage, ChangedOn = GETDATE(), ErrorCode = @ErrorCode, ImageId = @ImageID where JobId = @JobId
  
-                  INSERT INTO JobsDeliveryErrorsTracking(DeliveryErrorId,JobId,DeliveryRuleId,ErrorStatus,ErrorMessage,ExceptionMessage,FirstAttempt,ChangedOn,ErrorCode)
-                  SELECT DeliveryErrorId,JobId,DeliveryRuleId,ErrorStatus,ErrorMessage,ExceptionMessage,FirstAttempt,ChangedOn,ErrorCode FROM JobsDeliveryErrors where JobId = @JobId
+                  INSERT INTO JobsDeliveryErrorsTracking(DeliveryErrorId,JobId,DeliveryRuleId,ErrorStatus,ErrorMessage,ExceptionMessage,FirstAttempt,ChangedOn,ErrorCode,ImageID)
+                  SELECT DeliveryErrorId,JobId,DeliveryRuleId,ErrorStatus,ErrorMessage,ExceptionMessage,FirstAttempt,ChangedOn,ErrorCode,ImageID FROM JobsDeliveryErrors where JobId = @JobId
                   
             END
       END
