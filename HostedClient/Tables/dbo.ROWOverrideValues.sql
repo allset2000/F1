9debce1b0ea5d87248ce1c068ce2cfe754719e47
Raghu Arvapally <raghu.arvapally@valuelabs.com>
2016-02-25 19:46:35 +0530

    #393 Added by Baswaraj for Getting override active - added 3 columns

--- HostedClient/Tables/dbo.ROWOverrideValues.sql
+++ HostedClient/Tables/dbo.ROWOverrideValues.sql
@@ -2,7 +2,10 @@ CREATE TABLE [dbo].[ROWOverrideValues]
 (
 [JobNumber] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [FieldID] [smallint] NOT NULL,
-[Value] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+[Value] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[IsActive] [bit] NULL CONSTRAINT [DF_ROWOverrideValues_IsActive] DEFAULT ((1)),
+[CreatedBy] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CreatedDate] [datetime] NULL CONSTRAINT [DF_ROWOverrideValues_CreatedDate] DEFAULT (getdate())
 ) ON [PRIMARY]
 GO
 CREATE NONCLUSTERED INDEX [IX_JobNumber] ON [dbo].[ROWOverrideValues] ([JobNumber]) ON [PRIMARY]
