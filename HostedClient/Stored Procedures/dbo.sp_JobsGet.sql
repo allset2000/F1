2c7c41cfaa94e0fd2699f34ab2ad1a05961e5b7e
Sharif Shaik <sshaik@entradahealth.com>
2016-03-01 15:42:48 -0600

    #6057#

--- HostedClient/Stored Procedures/dbo.sp_JobsGet.sql
+++ HostedClient/Stored Procedures/dbo.sp_JobsGet.sql
@@ -8,6 +8,7 @@ GO
 --x  ver   |    date     |  by                 |  comments - include ticket#
 --x_____________________________________________________________________________
 --x   0    | 02/17/2016  | sharif shaik        | get image only jobs
+--X	  1	   | 03/01/2016	 | sharif shaik        | Changes for NextGen
 --xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 /*
 	set statistics io on
@@ -28,16 +29,38 @@ BEGIN
 
 	SET NOCOUNT ON;
 
-	SELECT Jobs.JobID, Jobs.JobNumber, JobTypes.Name as JobType, Encounters.AppointmentDate, 'pdf' as DocumentFormat,
-                                Patients.MRN, Patients.FirstName, Patients.LastName, Jobs.JobNumber AS ClientJobNumber, Schedules.AppointmentID, 
-                                Schedules.EHREncounterID, Schedules.ScheduleID, Jobs.JobID as HostedJobId, Jobs.EncounterID, Dictators.DictatorName as DictatorID, Dictators.EHRProviderAlias, Dictators.EHRProviderID,                                
-                                JobTypes.TddEnabled as IsExpressNote, JobTypes.EHRDocumentTypeID, JobTypes.EHRImageTypeId, JobTypes.AllowEncounterSearch, JobTypes.AllowNotifications, JobTypes.DocumentType, JobTypes.ACKEnabled
-                                FROM Jobs INNER JOIN JobTypes ON Jobs.JobTypeID = JobTypes.JobTypeID                                
-                                LEFT OUTER JOIN Encounters ON Encounters.EncounterID = Jobs.EncounterID
-                                INNER JOIN Patients ON Patients.PatientID = Encounters.PatientID                                 
-                                LEFT OUTER JOIN Schedules ON Schedules.ScheduleID = Encounters.ScheduleID
-                                LEFT OUTER JOIN Dictators ON Dictators.DictatorID = Jobs.OwnerDictatorID                                 
-                                WHERE Jobs.ClinicID = @ClinicID and Jobs.[Status] = @Status and ISNULL(jobs.hasImages, 0) = @HasImages and ISNULL(jobs.hasDictation, 0) = @HasDictation and ISNULL(jobs.HasChatHistory,0) = @HasChatHistory
-								--and Jobs.Jobid = 838920 -- Greenway 827790 --Athena 838920 --
+		SELECT Jobs.JobID
+			,Jobs.JobNumber
+			,JobTypes.Name as JobType
+			,Encounters.AppointmentDate
+			,'pdf' as DocumentFormat
+			,Patients.MRN
+			,Patients.FirstName
+			,Patients.LastName
+			,Jobs.JobNumber AS ClientJobNumber
+			,Schedules.AppointmentID
+			,Schedules.EHREncounterID
+			,Schedules.ScheduleID
+			,Jobs.JobID as HostedJobId
+			,Jobs.EncounterID
+			,Dictators.DictatorName as DictatorID
+			,Dictators.EHRProviderAlias
+			,Dictators.EHRProviderID
+			,JobTypes.TddEnabled as IsExpressNote
+			,JobTypes.EHRDocumentTypeID
+			,JobTypes.EHRImageTypeId
+			,JobTypes.AllowEncounterSearch
+			,JobTypes.AllowNotifications
+			,JobTypes.DocumentType
+			,JobTypes.ACKEnabled
+            ,Clinics.EHRClinicID as EHRPracticeID
+			FROM Jobs INNER JOIN JobTypes ON Jobs.JobTypeID = JobTypes.JobTypeID
+			INNER JOIN Clinics ON Clinics.ClinicID = Jobs.ClinicID AND Jobs.ClinicID = @ClinicID                                
+            LEFT OUTER JOIN Encounters ON Encounters.EncounterID = Jobs.EncounterID
+            INNER JOIN Patients ON Patients.PatientID = Encounters.PatientID                                 
+            LEFT OUTER JOIN Schedules ON Schedules.ScheduleID = Encounters.ScheduleID
+            LEFT OUTER JOIN Dictators ON Dictators.DictatorID = Jobs.OwnerDictatorID                                 
+            WHERE Jobs.[Status] = @Status and ISNULL(jobs.hasImages, 0) = @HasImages and ISNULL(jobs.hasDictation, 0) = @HasDictation and ISNULL(jobs.HasChatHistory,0) = @HasChatHistory
+			--and Jobs.Jobid = 838920 -- Greenway 827790 --Athena 838920 --
 END
 GO
