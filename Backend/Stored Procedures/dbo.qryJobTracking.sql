7fcc006ef289f438e62f583158c06aba21f88d2f
rnarender <ramadheni.narender@adroitent.com>
2016-03-09 17:16:16 +0530

    #6630# Updated SP "qryJobTracking" to get latest record from Jobs_Documents_History table while displaying in OLD portal job history view

--- Backend/Stored Procedures/dbo.qryJobTracking.sql
+++ Backend/Stored Procedures/dbo.qryJobTracking.sql
@@ -8,17 +8,17 @@ CREATE PROCEDURE [dbo].[qryJobTracking] (
 ) AS
 	SELECT dbo.JobTracking.JobNumber, dbo.JobTracking.Status , dbo.JobTracking.StatusDate , 
 			dbo.JobTracking.Path, dbo.StatusCodes.StatusName, dbo.StatusCodes.StatusClass, 
-			dbo.StatusCodes.StatusStage, dbo.Jobs_Documents_History.Username AS QAName,
-			dbo.Jobs_Documents_History.DocumentID ,dbo.Jobs_Documents.Username AS EditorName,
-			dbo.Jobs_Documents_History.Doc
+			dbo.StatusCodes.StatusStage, JDHH.Username AS QAName,
+			JDHH.DocumentID ,dbo.Jobs_Documents.Username AS EditorName,
+			JDHH.Doc
 	FROM   dbo.JobTracking INNER JOIN
 						  dbo.StatusCodes ON dbo.JobTracking.Status = dbo.StatusCodes.StatusID
 						  INNER JOIN
 						  dbo.Jobs_Documents ON dbo.JobTracking.JobNumber=dbo.Jobs_Documents.JobNumber						  
-						  LEFT OUTER JOIN
-						  dbo.Jobs_Documents_History ON dbo.JobTracking.JobNumber=dbo.Jobs_Documents_History.JobNumber 
-						  AND dbo.JobTracking.Status= dbo.Jobs_Documents_History.Status	
-						  AND dbo.JobTracking.StatusDate	=dbo.Jobs_Documents_History.StatusDate					 						  
+						  OUTER APPLY (SELECT TOP 1 Username, DocumentID, Doc from dbo.Jobs_Documents_History JDH 
+						  WHERE dbo.JobTracking.JobNumber= JDH.JobNumber 
+						  AND dbo.JobTracking.Status= JDH.Status	
+						  AND dbo.JobTracking.StatusDate = JDH.StatusDate ORDER BY JDH.DocDate ASC) JDHH		 						  
 	WHERE (dbo.JobTracking.JobNumber = @JobNumber) AND (dbo.JobTracking.Status in(100,160,180,210,360,240,250))
 	ORDER BY dbo.JobTracking.Status, dbo.JobTracking.StatusDate
 RETURN
