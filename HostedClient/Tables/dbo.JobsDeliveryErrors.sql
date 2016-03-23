32f3dbab210b28f2c322b2f611d83ccda8f1e65a
Santhosh <smukkamala@entradahealth.com>
2016-03-23 16:52:50 +0530

    #000# - Added Default Value & Not Null for ErrorCode Column

--- HostedClient/Tables/dbo.JobsDeliveryErrors.sql
+++ HostedClient/Tables/dbo.JobsDeliveryErrors.sql
@@ -8,13 +8,13 @@ CREATE TABLE [dbo].[JobsDeliveryErrors]
 [ExceptionMessage] [varchar] (250) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [FirstAttempt] [datetime] NOT NULL,
 [ChangedOn] [datetime] NOT NULL,
-[ErrorCode] [int] NULL,
+[ErrorCode] [int] NOT NULL CONSTRAINT [DF_JobsDeliveryErrors_ErrorCode] DEFAULT ((5000)),
 [ImageID] [bigint] NULL CONSTRAINT [DF_JobsDeliveryErrors_ImageID] DEFAULT (NULL)
 ) ON [PRIMARY]
-CREATE NONCLUSTERED INDEX [NonClusteredIndex-20160307-142703] ON [dbo].[JobsDeliveryErrors] ([ImageID]) ON [PRIMARY]
-
 GO
 ALTER TABLE [dbo].[JobsDeliveryErrors] ADD CONSTRAINT [PK_JobsDeliveryErrors] PRIMARY KEY CLUSTERED  ([DeliveryErrorId]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [NonClusteredIndex-20160307-142703] ON [dbo].[JobsDeliveryErrors] ([ImageID]) ON [PRIMARY]
 GO
 CREATE NONCLUSTERED INDEX [IX_JobsDeliveryErrors_JobId_ErrorStatus_ErrorMessage] ON [dbo].[JobsDeliveryErrors] ([JobId], [ErrorStatus], [ErrorMessage]) ON [PRIMARY]
 GO
