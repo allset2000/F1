a5e0665ed3e860588d8fd6c4f8c2c60984a2a125
Raghu Arvapally <raghu.arvapally@valuelabs.com>
2016-03-23 20:49:40 +0530

    #6915,7068# bugs fixed

--- HostedClient/Stored Procedures/dbo.sp_GetJobsToSync.sql
+++ HostedClient/Stored Procedures/dbo.sp_GetJobsToSync.sql
@@ -15,23 +15,28 @@ GO
 --              The main select statment can be used by other sql objects. 
 --              Therefore, to easy the maintenability, we will keep it separate.
 
---	Modified By: Mikayil Bayramov
---	Modification Date: 5/29/2015
---	Details: Performance improvement. Eliminating use of vw_GetJobsToSync view.
+--	Modified By: Mikayil Bayramov,Raghu A
+--	Modification Date: 5/29/2015,3/23/2016
+--	Details: Performance improvement. Eliminating use of vw_GetJobsToSync view.,   
+--           jobtypecategpryID condition added for compatibility with Android and IOS  Old version(release E)
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_GetJobsToSync](
 	 @DictatorId INT,
 	 @MaxFutureDays INT
 ) AS 
 BEGIN
-	SELECT j.JobID, j.JobNumber, j.ClinicID, j.EncounterID, j.JobTypeID, j.OwnerDictatorID, j.[Status], j.Stat, j.[Priority], j.RuleID, j.AdditionalData 
+	SELECT j.JobID, j.JobNumber, j.ClinicID, j.EncounterID, j.JobTypeID, j.OwnerDictatorID, j.[Status],
+	       j.Stat, j.[Priority], j.RuleID, j.AdditionalData 
 	FROM dbo.Dictations AS d INNER JOIN dbo.Jobs AS j ON d.JobID = j.JobID 
 						     INNER JOIN dbo.Encounters AS e ON j.EncounterID = e.EncounterID
 						     INNER JOIN dbo.Queue_Users AS qu ON qu.QueueID = d.QueueID
 						     INNER JOIN dbo.Queues AS q ON q.QueueID = qu.QueueID
-	WHERE qu.DictatorID = @DictatorId AND 	       
+							 INNER JOIN dbo.JobTypes jt on jt.JobTypeID=j.JobTypeID
+	WHERE qu.DictatorID = @DictatorId AND 
+	      jt.JobTypeCategoryId=1 AND   
 		  d.[Status] IN (100, 200) AND 
 		  q.Deleted = 0 AND 
 		  DATEDIFF (D, GETDATE(), e.AppointmentDate) <= @MaxFutureDays 
 END
+
 GO
