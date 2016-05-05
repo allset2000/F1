837b5cde2f1affb70f83046b2ed7410d0f7b27c3
Raghu Arvapally <raghu.arvapally@valuelabs.com>
2016-05-05 21:51:38 +0530

    #7952# bug fixing

--- Backend/Stored Procedures/dbo.spv_MoveJobToNewStatus.sql
+++ Backend/Stored Procedures/dbo.spv_MoveJobToNewStatus.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -8,6 +9,7 @@ GO
 -- Description:	<Proc to Move JobStatus on backend DB>
 --				Designed only for 280-->345, 280-->354 and 345-->354. To be called from EL and Downloader.
 --				Can be used for other status movement but check the code and test it.
+--exec spv_MoveJobToNewStatus '2016050500000002',354
 -- =============================================
 CREATE PROCEDURE [dbo].[spv_MoveJobToNewStatus] 
 	@JobNumber varchar(20),
@@ -18,7 +20,7 @@ BEGIN
 	DECLARE @CurrentJobStatus smallint
 	DECLARE @Path varchar(200)	--JobTracking takes 255 but JobStatusA and JobStatusB has only 200.
 
-	SELECT @CurrentJobStatus = Status, @Path = Path from JobstatusB  
+	SELECT @CurrentJobStatus = Status, @Path = Path from JobstatusB WHERE JobNumber=@JobNumber 
 
 	-- status hasn't changed so nothing to update
 	IF @CurrentJobStatus = @NewJobStatus 
@@ -26,11 +28,12 @@ BEGIN
 
 	--Designed only for 280-->345, 280-->354 and 345-->354. To be called from EL and Downloader.
 	--Can be used for other status movement but check the code and test it.
-	IF @NewJobStatus <> 345 and @NewJobStatus <> 354
+	IF (@NewJobStatus NOT IN(345,354))
 		RETURN; 
 
 	BEGIN TRY
 		BEGIN TRANSACTION
+
 			UPDATE Jobs SET 
 					JobStatus = @NewJobStatus, 
 					ProcessFailureCount = 0, 
@@ -47,6 +50,7 @@ BEGIN
 
 			INSERT INTO JobTracking (JobNumber, Status, StatusDate, Path)
 				VALUES(@JobNumber, @NewJobStatus, GetDate(), @Path)
+				     
 
 		COMMIT TRANSACTION
 	 END TRY
