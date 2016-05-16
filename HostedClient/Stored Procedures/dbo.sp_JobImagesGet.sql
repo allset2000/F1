bf6c29c848a99a0f33b68bb0258c54e98bf7f98b
srachabandi <suresh.rachabandi@adroitent.com>
2016-05-16 13:47:51 +0530

    #5882# spassing row retry duration value to sp

--- HostedClient/Stored Procedures/dbo.sp_JobImagesGet.sql
+++ HostedClient/Stored Procedures/dbo.sp_JobImagesGet.sql
@@ -16,9 +16,10 @@ GO
 	-- Athena: 838920
 */
 
-CREATE PROCEDURE [dbo].[sp_JobImagesGet] 
-	@JobID bigint	
-	
+CREATE PROCEDURE [dbo].[sp_JobImagesGet]( 
+	@JobID bigint,
+	@rowRetryDuration int	
+	)
 AS
 BEGIN
 
@@ -31,7 +32,7 @@ BEGIN
 	SELECT JobImages.ImageID, JobImages.JobID, JobImages.ImagePath, JobImages.ImageDescription  
 	FROM JobImages WITH (NOLOCK)
 	LEFT JOIN JobsDeliveryTracking on JobImages.ImageID = JobsDeliveryTracking.ImageID 
-	LEFT JOIN JobsDeliveryErrors on JobImages.ImageID = JobsDeliveryErrors.ImageID AND DATEDIFF(minute,ChangedOn,GETDATE()) < 30
+	LEFT JOIN JobsDeliveryErrors on JobImages.ImageID = JobsDeliveryErrors.ImageID AND DATEDIFF(minute,ChangedOn,GETDATE()) < @rowRetryDuration
 	WHERE JobImages.JobID = @JobID AND JobsDeliveryTracking.JobID IS NULL 
 	AND JobsDeliveryErrors.JobID IS NULL 
 
