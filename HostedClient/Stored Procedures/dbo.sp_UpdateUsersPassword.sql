b1630b7b9fa0dc4c5bedd441f86e8f958045cfab
Raghu Arvapally <raghu.arvapally@valuelabs.com>
2016-05-16 22:47:01 +0530

    Revert "#000# SP updated for insert data into UserPasswordHistory table"
    This reverts commit 4a9e00702f7c92c28367ac3a05f5c67e7cfc1da2.

--- HostedClient/Stored Procedures/dbo.sp_UpdateUsersPassword.sql
+++ HostedClient/Stored Procedures/dbo.sp_UpdateUsersPassword.sql
@@ -1,4 +1,3 @@
-
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -7,7 +6,6 @@ GO
 -- Author: Sam Shoultz
 -- Create date: 01/21/2015
 -- Description: SP used to Update the User account and reset the password
---Modified : Raghu A -- 05/16/2016 -- updated in Password history table 
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_UpdateUsersPassword]
 (
@@ -17,29 +15,8 @@ CREATE PROCEDURE [dbo].[sp_UpdateUsersPassword]
 )
 AS
 BEGIN
+	UPDATE Users SET Password = @Password, Salt = @Salt WHERE UserId = @UserId
 
-  SET NOCOUNT ON;  
-
-	BEGIN TRY 
-		BEGIN TRANSACTION 
-			UPDATE dbo.Users SET Password = @Password, Salt = @Salt,LastPasswordReset=NULL,SecurityToken='',IsLockedOut=0 WHERE UserId = @UserId
-	
-			INSERT INTO [UserPasswordHistory] 
-				  (UserId,Password,Salt,IsActive,DateCreated)
-			VALUEs
-				  (@UserId,@Password,@Salt,1,GETDATE())
-
-			SELECT * FROM Users where UserId = @UserId
-	COMMIT TRANSACTION
-	END TRY
-	BEGIN CATCH
-		IF @@TRANCOUNT > 0 
-		   BEGIN
-				ROLLBACK TRANSACTION
-				DECLARE @ErrMsg nvarchar(4000), @ErrSeverity int
-				SELECT @ErrMsg = ERROR_MESSAGE(), @ErrSeverity = ERROR_SEVERITY()
-				RAISERROR(@ErrMsg, @ErrSeverity, 1)
-			END
-	END CATCH 
+	SELECT * FROM Users where UserId = @UserId
 END
 GO
