eb4442749c554f27b962bfd577d052f4d0f9e369
Santhosh <smukkamala@entradahealth.com>
2016-05-19 18:53:04 +0530

    #8064# - Updating Admin Console SP character limit

--- HostedClient/Tables/dbo.PermissionChangeTracking.sql
+++ HostedClient/Tables/dbo.PermissionChangeTracking.sql
@@ -1,9 +1,9 @@
 CREATE TABLE [dbo].[PermissionChangeTracking]
 (
 [PermissionChangeTrackingId] [int] NOT NULL IDENTITY(1, 1),
-[AddPermissions] [varchar] (1000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[DelPermissions] [varchar] (1000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[AppPermissions] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[AddPermissions] [varchar] (5000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[DelPermissions] [varchar] (5000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[AppPermissions] [varchar] (1000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [ChangedBy] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [DateChanged] [datetime] NULL
 ) ON [PRIMARY]
