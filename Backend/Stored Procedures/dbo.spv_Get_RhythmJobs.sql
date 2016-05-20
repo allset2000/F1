04fdb6f0a8811f706c016e250c7ec0405923df9a
nagaraja-soubrayalou <nsoubrayalou@entradahealth.com>
2016-05-20 10:41:07 -0500

    #5457 - Fixed the issue with incorrect parathesis in WHERE clause

--- Backend/Stored Procedures/dbo.spv_Get_RhythmJobs.sql
+++ Backend/Stored Procedures/dbo.spv_Get_RhythmJobs.sql
@@ -1,3 +1,4 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -27,6 +28,7 @@ GO
 --X  VER   |    DATE      |  BY						|  COMMENTS - include Ticket#
 --X_____________________________________________________________________________
 --X   0    | 04/28/2016   | Naga					| Initial Design
+--X   1    | 05/20/2016   | Naga					| Fixed the issue with incorrect parathesis in WHERE clause
 --XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX	
 CREATE PROCEDURE [dbo].[spv_Get_RhythmJobs]
 				@ProcessFailureCountMax SMALLINT
@@ -38,7 +40,7 @@ BEGIN
 	SELECT j.JobNumber, j.JobId, j.DictatorID, j.DictationDate, j.JobType, j.ClinicID, ja.[Status] AS [JobStatus], j.TemplateName FROM dbo.Jobs j
 	INNER JOIN dbo.JobStatusA ja
 		ON j.JobNumber = ja.JobNumber
-	WHERE (j.RhythmWorkFlowID = 1 AND j.JobStatus = 138) OR (j.RhythmWorkFlowID = 3 AND j.JobStatus = 275)
+	WHERE ((j.RhythmWorkFlowID = 1 AND j.JobStatus = 138) OR (j.RhythmWorkFlowID = 3 AND j.JobStatus = 275))
 		AND ISNULL(j.ProcessFailureCount, 0) < @ProcessFailureCountMax
 
 
