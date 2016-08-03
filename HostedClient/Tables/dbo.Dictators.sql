6847f5f7596e2b722fbadacb1d07c509dbc5dd89
unknown <mmartin@ENTRADAHEALTH>
2016-08-03 18:45:11 -0500

    Add Prod Changes to SOurce

--- HostedClient/Tables/dbo.Dictators.sql
+++ HostedClient/Tables/dbo.Dictators.sql
@@ -16,7 +16,7 @@ CREATE TABLE [dbo].[Dictators]
 [EHRProviderID] [varchar] (36) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [EHRProviderAlias] [varchar] (36) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [DefaultQueueID] [int] NULL,
-[VRMode] [smallint] NOT NULL CONSTRAINT [DF__Dictators__VRMod__4E1E9780] DEFAULT ((99)),
+[VRMode] [smallint] NOT NULL CONSTRAINT [DF_Dictators_VRMode] DEFAULT ((99)),
 [CRFlagType] [int] NOT NULL CONSTRAINT [DF_Dictators_CRFlagType] DEFAULT ((0)),
 [ForceCRStartDate] [datetime] NULL,
 [ForceCREndDate] [datetime] NULL,
@@ -29,61 +29,44 @@ CREATE TABLE [dbo].[Dictators]
 [AppSetting_DisableSendToTranscription] [bit] NOT NULL CONSTRAINT [DF_Dictators_AppSetting_DisableSendToTranscription] DEFAULT ((0)),
 [Preference] [varchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-ALTER TABLE [dbo].[Dictators] ADD
-CONSTRAINT [FK_Dictators_RhythmWorkFlows] FOREIGN KEY ([RhythmWorkFlowID]) REFERENCES [dbo].[RhythmWorkFlows] ([RhythmWorkFlowID])
 GO
 ALTER TABLE [dbo].[Dictators] ADD CONSTRAINT [PK_Dictators] PRIMARY KEY CLUSTERED  ([DictatorID]) ON [PRIMARY]
-
+GO
+CREATE NONCLUSTERED INDEX [IX_Dictators_ClinicID] ON [dbo].[Dictators] ([ClinicID], [EHRProviderID]) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_Dictators_DictatorName] ON [dbo].[Dictators] ([DictatorName]) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Dictators] ADD CONSTRAINT [IX_Dictators_Unique_DictatorName] UNIQUE NONCLUSTERED  ([DictatorName], [ClinicID]) ON [PRIMARY]
-
-CREATE NONCLUSTERED INDEX [IX_Dictators_ClinicID] ON [dbo].[Dictators] ([ClinicID], [EHRProviderID]) ON [PRIMARY]
-
-CREATE NONCLUSTERED INDEX [IX_Dictators_DictatorName] ON [dbo].[Dictators] ([DictatorName]) ON [PRIMARY]
-
+GO
+ALTER TABLE [dbo].[Dictators] ADD CONSTRAINT [FK_Dictators_Clinics] FOREIGN KEY ([ClinicID]) REFERENCES [dbo].[Clinics] ([ClinicID])
+GO
+ALTER TABLE [dbo].[Dictators] ADD CONSTRAINT [FK_Dictators_CRFlagTypes] FOREIGN KEY ([CRFlagType]) REFERENCES [dbo].[CRFlagTypes] ([CRFlagType])
+GO
+ALTER TABLE [dbo].[Dictators] ADD CONSTRAINT [FK_Dictators_JobTypes] FOREIGN KEY ([DefaultJobTypeID]) REFERENCES [dbo].[JobTypes] ([JobTypeID])
+GO
+ALTER TABLE [dbo].[Dictators] ADD CONSTRAINT [FK_Dictators_RhythmWorkFlows] FOREIGN KEY ([RhythmWorkFlowID]) REFERENCES [dbo].[RhythmWorkFlows] ([RhythmWorkFlowID])
+GO
+ALTER TABLE [dbo].[Dictators] ADD CONSTRAINT [fk_dictators_SRETypeId] FOREIGN KEY ([SRETypeId]) REFERENCES [dbo].[SREEngineType] ([SRETypeId])
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Default Job Type', 'SCHEMA', N'dbo', 'TABLE', N'Dictators', 'COLUMN', N'DefaultJobTypeID'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Client UserID', 'SCHEMA', N'dbo', 'TABLE', N'Dictators', 'COLUMN', N'DictatorID'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'BackEnd Dictator Name', 'SCHEMA', N'dbo', 'TABLE', N'Dictators', 'COLUMN', N'DictatorName'
 GO
 
 SET ANSI_PADDING OFF
 GO
 
-ALTER TABLE [dbo].[Dictators]  WITH CHECK ADD  CONSTRAINT [FK_Dictators_Clinics] FOREIGN KEY([ClinicID])
-REFERENCES [dbo].[Clinics] ([ClinicID])
-GO
-
 ALTER TABLE [dbo].[Dictators] CHECK CONSTRAINT [FK_Dictators_Clinics]
 GO
 
-ALTER TABLE [dbo].[Dictators]  WITH CHECK ADD  CONSTRAINT [FK_Dictators_CRFlagTypes] FOREIGN KEY([CRFlagType])
-REFERENCES [dbo].[CRFlagTypes] ([CRFlagType])
-GO
-
 ALTER TABLE [dbo].[Dictators] CHECK CONSTRAINT [FK_Dictators_CRFlagTypes]
-GO
-
-ALTER TABLE [dbo].[Dictators]  WITH CHECK ADD  CONSTRAINT [FK_Dictators_JobTypes] FOREIGN KEY([DefaultJobTypeID])
-REFERENCES [dbo].[JobTypes] ([JobTypeID])
 GO
 
 ALTER TABLE [dbo].[Dictators] CHECK CONSTRAINT [FK_Dictators_JobTypes]
 
 GO
 
-ALTER TABLE [dbo].[Dictators]  WITH CHECK ADD  CONSTRAINT [fk_dictators_SRETypeId] FOREIGN KEY([SRETypeId])
-REFERENCES [dbo].[SREEngineType] ([SRETypeId])
-GO
-
 ALTER TABLE [dbo].[Dictators] CHECK CONSTRAINT [fk_dictators_SRETypeId]
-GO
-
-
---EXEC sp_addextendedproperty N'MS_Description', N'Default Job Type', 'SCHEMA', N'dbo', 'TABLE', N'Dictators', 'COLUMN', N'DefaultJobTypeID'
---GO
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Client UserID' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Dictators', @level2type=N'COLUMN',@level2name=N'DictatorID'
-
-GO
-
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'BackEnd Dictator Name' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Dictators', @level2type=N'COLUMN',@level2name=N'DictatorName'
-GO
-
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Default Job Type' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Dictators', @level2type=N'COLUMN',@level2name=N'DefaultJobTypeID'
 GO
